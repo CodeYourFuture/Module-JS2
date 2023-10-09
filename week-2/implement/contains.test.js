bea23e72d728e40e43977ae7a5129e78fe96d2c8
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-10-09 14:52:57 +0100

    add sketch of object exercises

---
+++ week-2/implement/contains.test.js
@@ -0,0 +1,27 @@
+/**
+ * contains
+ *
+ * Acceptance criteria:
+ *
+ * Given a contains function
+ * When passed an object and a property name
+ * Then it should return true if the object contains the property, false otherwise
+ *
+ * Given an empty object
+ * When passed to contains
+ * Then it should return false
+ *
+ * Given an object with properties
+ * When passed to contains with an existing property name
+ * Then it should return true
+ *
+ * Given an object with properties
+ * When passed to contains with a non-existent property name
+ * Then it should return false
+ *
+ * Given invalid parameters like arrays
+ * When passed to contains
+ * Then it should return false or throw an error
+ *
+ *
+ */
---
+++ week-2/interpret/invert.js
@@ -0,0 +1,9 @@
+function invert(obj) {
+  const invertedObj = {};
+
+  for (const [key, value] of Object.entries(obj)) {
+    invertedObj.key = value;
+  }
+
+  return invertedObj;
+}
