58c1ddbb9167b51122f1096438d71c7ebf50f69c
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-10-12 12:14:36 +0100

    update function name

--- week-1/fix/median.js
+++ week-1/fix/median.js
@@ -1,9 +1,10 @@
-function calcualeMedian(list) {
-  // implementation
+function calculateMedian(list) {
+  // fix this implementation
+  // start by running the tests for this function
 
   const middleIndex = Math.floor(list.length / 2);
   const median = list.splice(middleIndex, 1)[0];
   return median;
 }
 
-module.exports = calcualeMedian;
+module.exports = calculateMedian;
