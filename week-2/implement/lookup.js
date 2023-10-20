1f784683f7b882d3899e650134dc3be467c378e3
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-10-20 15:13:55 +0100

    improve lookup problem

--- week-2/implement/lookup.js
+++ week-2/implement/lookup.js
@@ -1,33 +1,3 @@
-/**
- * Acceptance Criteria for createLookup function
- *
- * Given
- * - An array of arrays representing country code and currency code pairs
- *   e.g. [['US', 'USD'], ['CA', 'CAD']]
- *
- * When
- * - createLookup function is called with the country-currency array as an argument
- *
- * Then
- * - It should return an object where:
- *   - The keys are the country codes
- *   - The values are the corresponding currency codes
- *
- * Example
- * Given:
- * [['US', 'USD'], ['CA', 'CAD']]
- *
- * When
- * createLookup(countryCurrencyPairs) is called
- *
- * Then
- * It should return:
- * {
- *   'US': 'USD',
- *   'CA': 'CAD'
- * }
- */
-
 function createLookup() {
   // implementation here
 }
@@ -44,17 +14,4 @@ test("converts a single pair of currency codes", () => {
   });
 });
 
-test("creates a country currency code lookup for multiple codes", () => {
-  const COUNTRY_CURRENCY_CODES = [
-    ["GB", "GBP"],
-    ["DE", "EUR"],
-    ["NG", "NGN"],
-    ["MX", "MXN"],
-  ];
-  expect(createLookup(COUNTRY_CURRENCY_CODES)).toEqual({
-    GB: "GBP",
-    DE: "EUR",
-    NG: "NGN",
-    MX: "MXN",
-  });
-});
+test.todo("creates a country currency code lookup for multiple codes");
---
+++ week-2/implement/lookup.test.js
@@ -0,0 +1,31 @@
+/*
+
+Create a lookup object of key value pairs from an array of code pairs
+
+Acceptance Criteria:
+
+Given
+ - An array of arrays representing country code and currency code pairs
+   e.g. [['US', 'USD'], ['CA', 'CAD']]
+
+When
+ - createLookup function is called with the country-currency array as an argument
+
+Then
+ - It should return an object where:
+   - The keys are the country codes
+   - The values are the corresponding currency codes
+
+Example
+Given: [['US', 'USD'], ['CA', 'CAD']]
+
+When
+createLookup(countryCurrencyPairs) is called
+
+Then
+It should return:
+ {
+   'US': 'USD',
+   'CA': 'CAD'
+ }
+*/
