22fee720d0d2538b12cab6baa9ccb914e942e6a1
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-10-20 15:09:24 +0100

    improve array acceptance criteria format

--- week-1/implement/sum.test.js
+++ week-1/implement/sum.test.js
@@ -4,27 +4,26 @@ In this kata, you will need to implement a function that sums the numerical elem
 
 E.g. sum([10, 20, 30]), target output: 60
 E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical elements)
-
-Acceptance Criteria:
-
-Given an empty array
-When passed to the sum function
-Then it should return 0
-
-Given an array with just one number
-When passed to the sum function
-Then it should return that number
-
-Given an array containing negative numbers
-When passed to the sum function
-Then it should still return the correct total sum
-
-Given an array with decimal/float numbers
-When passed to the sum function
-Then it should return the correct total sum
-
-Given an array containing non-number values
-When passed to the sum function
-Then it should ignore the non-numerical values and return the sum of the numerical elements
-
 */
+
+// Acceptance Criteria:
+
+// Given an empty array
+// When passed to the sum function
+// Then it should return 0
+
+// Given an array with just one number
+// When passed to the sum function
+// Then it should return that number
+
+// Given an array containing negative numbers
+// When passed to the sum function
+// Then it should still return the correct total sum
+
+// Given an array with decimal/float numbers
+// When passed to the sum function
+// Then it should return the correct total sum
+
+// Given an array containing non-number values
+// When passed to the sum function
+// Then it should ignore the non-numerical values and return the sum of the numerical elements
