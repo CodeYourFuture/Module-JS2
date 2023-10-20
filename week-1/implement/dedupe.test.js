a89a8779cec1f33bae49577def16a6dbdc3c362e
Dedekind561 <mitchell.lloyd154@gmail.com>
2023-10-20 14:16:26 +0100

    tidy up acceptance criteria for array katas

--- week-1/implement/dedupe.test.js
+++ week-1/implement/dedupe.test.js
@@ -1,28 +1,28 @@
 /*
- * Dedupe Array
- *
- * Dedupe means deduplicate
- * In this kata, you will need to deduplicate the elements of an array
- *
- * E.g. dedupe(['a','a','a','b','b','c']) target output: ['a','b','c']
- * E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) target output: [5, 1, 2, 3, 8]
- *
- * Acceptance Criteria:
- *
- * Given an empty array
- * When passed to the dedupe function
- * Then it should return an empty array
- *
- * Given an array with no duplicates
- * When passed to the dedupe function
- * Then it should return a copy of the original array
- *
- * Given an array with duplicate primitive values like strings or numbers
- * When passed to the dedupe function
- * Then it should remove the duplicate values
- *
- * Given an array with duplicate object references
- * When passed to the dedupe function
- * Then it should remove the duplicate references
- *
- */
+Dedupe Array
+
+Dedupe means deduplicate
+In this kata, you will need to deduplicate the elements of an array
+
+E.g. dedupe(['a','a','a','b','b','c']) target output: ['a','b','c']
+E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) target output: [5, 1, 2, 3, 8]
+
+Acceptance Criteria:
+
+Given an empty array
+When passed to the dedupe function
+Then it should return an empty array
+
+Given an array with no duplicates
+When passed to the dedupe function
+Then it should return a copy of the original array
+
+Given an array with duplicate primitive values like strings or numbers
+When passed to the dedupe function
+Then it should remove the duplicate values
+
+Given an array with duplicate object references
+When passed to the dedupe function
+Then it should remove the duplicate references
+
+*/
--- week-1/implement/max.test.js
+++ week-1/implement/max.test.js
@@ -1,33 +1,28 @@
-// find max of an array of numbers
+/* Find maximum element of an array of numbers
 
-// Given a function called max
-// When passed an array of numbers
-// Then it should return the largest number in the array
+In this kata, you will need to implement a function that sums the numerical elements of an array
 
-// Given an empty array
-// When passed to the max function
-// Then it should return -Infinity
+E.g. max([30, 50, 10, 40]), target output: 50
+E.g. max(['hey', 10, 'hi', 60, 10]), target output: 60 (sum ignores any non-numerical elements)
 
-// Given an array with one number
-// When passed to the max function
-// Then it should return that number
+Given an empty array
+When passed to the max function
+Then it should return -Infinity
 
-// Given an array with positive numbers
-// When passed to the max function
-// Then it should return the largest positive number
+Given an array with one number
+When passed to the max function
+Then it should return that number
 
-// Given an array with negative numbers
-// When passed to the max function
-// Then it should return the largest negative number
+Given an array with both positive and negative numbers
+When passed to the max function
+Then it should return the largest number overall
 
-// Given an array with both positive and negative numbers
-// When passed to the max function
-// Then it should return the largest number overall
+Given an array with decimal numbers
+When passed to the max function
+Then it should return the largest decimal number
 
-// Given an array with decimal numbers
-// When passed to the max function
-// Then it should return the largest decimal number
+Given an array with non-number values
+When passed to the max function
+Then it should return the max and ignore non-numeric values
 
-// Given an array with non-number values
-// When passed to the max function
-// Then it should return the max and ignore non-numeric values
+*/
