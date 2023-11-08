b588fc7ac643876658e37fee53f738275e7abbde
bahador <noor.bahadory786@gmail.com>
2023-11-08 02:27:50 +0000

    completed interpret exercise

--- week-2/interpret/invert.js
+++ week-2/interpret/invert.js
@@ -8,20 +8,22 @@
 
 function invert(obj) {
   const invertedObj = {};
-
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
-
+console.log(invert({ a: 1, b: 2 }));
 // a) What is the current return value when invert is called with { a : 1 }
+// {'1': 'a'}
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+// {'1': 'a', '2': 'b'}
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
+// {'1': 'a', '2': 'b'}
 
 // c) What does Object.entries return? Why is it needed in this program?
-
+// It returns an array containing arrays of key-value pairs of an object's enumerable
 // d) Explain why the current return value is different from the target output
