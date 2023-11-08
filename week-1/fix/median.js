c15ab785ffbc453f60e6725b0608cd2f8ab38f7f
Elmira Zangeneh <elmira.zangeneh89@gmail.com>
2023-11-08 12:48:37 +0000

    edit dedupe

--- week-1/fix/median.js
+++ week-1/fix/median.js
@@ -2,19 +2,35 @@
 // Start by running the tests for this function
 // If you're in the week-1 directory, you can run npm test -- fix to run the tests in the fix directory
 
-function calculateMedian(list) {
-  //sub goal=find the middle position of the numbers 
-  // Step 1: Create a copy of the original array
-  const copiedList = [...list];
-  // Step 2: Find the middle index of the sorted array
-  const middleIndex = Math.floor(copiedList.length / 2);
-  // Step 3: Determine the median
-  const median = copiedList.length % 2 === 0
-  ? (copiedList[middleIndex - 1] + copiedList[middleIndex]) / 2
-  : copiedList[middleIndex];
-  // const median = list.splice(middleIndex, 1)[0];
-  return median;
+// function calculateMedian(list) {
+//   //sub goal=find the middle position of the numbers 
+//   // Step 1: Create a copy of the original array
+//   const copiedList = [...list];
+//   // Step 2: Find the middle index of the sorted array
+//   const middleIndex = Math.floor(copiedList.length / 2);
+//   // Step 3: Determine the median
+//   const median = copiedList.length % 2 === 0
+//   ? (copiedList[middleIndex - 1] + copiedList[middleIndex]) / 2
+//   : copiedList[middleIndex];
+//   // const median = list.splice(middleIndex, 1)[0];
+//   return median;
+// }
+function calculateMedian(list){
+  const sortedList = [...list];
+  sortedList.sort((a, b) => a - b);
+  const middleIndex = Math.floor(sortedList.length / 2);
+  if(sortedList.length % 2 === 0){//If even, return the average of the two middle values
+    const middleValue1 = sortedList[middleIndex - 1];
+    const middleValue2 = sortedList[middleIndex];
+    return (middleValue1 + middleValue2) / 2; 
+  }
+  else { // If odd 
+    return sortedList[middleIndex];
+   }
 }
+const myList = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]; const median = calculateMedian(myList); console.log("Median:", median); 
+  
+
 
 module.exports = calculateMedian;
 //export the function:make the function available for use in other files in other directory as well
--- week-1/implement/dedupe.test.js
+++ week-1/implement/dedupe.test.js
@@ -24,3 +24,25 @@ test.todo("given an empty array, it returns an empty array");
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values
+
+describe('dedupe', () => {
+    it('removes duplicates from an array', () => {
+      const inputArray = [1, 2, 2, 3, 4, 4, 5];
+      const expectedResult = [1, 2, 3, 4, 5];
+      expect(dedupe(inputArray)).toEqual(expectedResult);
+    });
+  
+    it('handles an array with no duplicates', () => {
+      const inputArray = [1, 2, 3, 4, 5];
+      const expectedResult = [1, 2, 3, 4, 5];
+      expect(dedupe(inputArray)).toEqual(expectedResult);
+    });
+  
+    it('handles an empty array', () => {
+      const inputArray = [];
+      const expectedResult = [];
+      expect(dedupe(inputArray)).toEqual(expectedResult);
+    });
+  });
+  
+   
