a477127495d7d4a9c9df7b02c1313805f8fd08ed
Sariat <adenike.sariat@gmail.com>
2023-11-09 14:32:20 +0000

    tested the function using console

--- week-1/implement/sum.js
+++ week-1/implement/sum.js
@@ -8,9 +8,9 @@ function sum(arr) {
   return mySum;
 }
 
-module.exports = sum;
+console.log(sum([4, 5, "y", 7, "yuo"]));
+console.log(sum([-3, -34, -6, -8]));
+console.log(sum([9.4, 5.2, 7.5, 11.7]));
+console.log(sum([]));
 
-// console.log(sum([4, 5, "y", 7, "yuo"]));
-// console.log(sum([-3, -34, -6, -8]));
-// console.log(sum([9.4, 5.2, 7.5, 11.7]));
-// console.log(sum([]));
+module.exports = sum;
