cc496d9e4d9894a5b128ca6e9ab0d2cce3f6e549
Sariat <adenike.sariat@gmail.com>
2023-11-09 11:25:13 +0000

    using for loop and if statement to test

--- week-1/implement/max.js
+++ week-1/implement/max.js
@@ -2,5 +2,11 @@ function max(list) {
   const maxNumber = [];
   if (list.length === 0) {
     return "-Infinity";
+  } else {
+    for (const item of list) {
+      if (typeof item === "number") {
+        maxNumber.push(item);
+      }
+    }
   }
 }
