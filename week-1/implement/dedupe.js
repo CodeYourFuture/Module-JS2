ff04ec9c26fa316e4afd4ae96c236d5a452514b3
Tajudeen Oyehan <oyehan.uob@gmail.com>
2023-11-10 00:28:10 +0000

    dedupe function and test completed

--- week-1/implement/dedupe.js
+++ week-1/implement/dedupe.js
@@ -1,1 +1,15 @@
-function dedupe() {}
+function dedupe(arr) {
+    const newArr = [];
+    for (const item of arr){
+        if (!newArr.includes(item)){
+            newArr.push(item);
+        }
+    }return newArr;
+}
+
+console.log(dedupe(["a", "a", "a", "b", "b", "c"]));
+console.log(dedupe([5, 1, 1, 2, 3, 2, 5, 8]));
+console.log(dedupe([]));
+const mixedArr = ["a", "d", 2, 3, 2, "d", 1, 1, "e"];
+console.log(dedupe(mixedArr));
+module.exports = dedupe;
