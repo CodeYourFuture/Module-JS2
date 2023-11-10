09a5771e690d2350227a00039fde29a5f570e650
Sariat <adenike.sariat@gmail.com>
2023-11-10 11:20:15 +0000

    I refactorised the implemetation to use for of loop

--- week-1/refactor/find.js
+++ week-1/refactor/find.js
@@ -1,13 +1,13 @@
 // Refactor the implementation of find to use a for...of loop
 
 function find(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
+  for (const element of list) {
+    if(element === target){
+      index = list.indexOf(element);
       return index;
     }
   }
   return -1;
 }
-
+console.log(find(["a", "b", "c", "d"], "c"));
 module.exports = find;
