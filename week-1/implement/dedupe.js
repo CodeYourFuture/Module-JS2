7cdb9ceb51f2d03e201e9dc9969a56c8bd8380dd
Sariat <adenike.sariat@gmail.com>
2023-11-10 10:06:58 +0000

    Created the function and used .includes

--- week-1/implement/dedupe.js
+++ week-1/implement/dedupe.js
@@ -1,1 +1,9 @@
-function dedupe() {}
+function dedupe(arr) {
+    let newArr = [];
+    for (const item of arr){
+        if (!newArr.includes(item)) {
+            newArr.push(item);
+        }
+    }
+    return newArr;
+}
