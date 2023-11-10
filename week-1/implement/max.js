60410d58bbd6f9e6968c959f79a24bdf42dd0b91
karam <karam@karams-MacBook-Pro.local>
2023-11-10 19:42:53 +0000

    completed implement and refactor exercises

---
+++ week-1/implement/max.js
@@ -0,0 +1,14 @@
+function max(numbers) {
+    const largestNumber = Math.max(...numbers);
+    for (element of numbers) {
+      if (typeof element === "string") {
+        const arrWithoutStrings = numbers.filter(
+          (strings) => typeof strings === "number"
+        );
+        return Math.max(...arrWithoutStrings);
+      }
+    }
+    return largestNumber;
+  }
+  
+  module.exports = max;
