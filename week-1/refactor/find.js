4b9d84296fc859fa4d5daaec0bc2fb8d6410af94
Tajudeen Oyehan <oyehan.uob@gmail.com>
2023-11-10 01:02:45 +0000

    refactor solved

--- week-1/refactor/find.js
+++ week-1/refactor/find.js
@@ -10,10 +10,12 @@
 //   return -1;
 // }
 function find(list, target){
+  let index = 0;
   for (const element of list){
     if (element === target){
       return index;
     }
+  index++
   }
   return -1;
 }
