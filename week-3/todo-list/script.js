dbe05effb51240a576f1cec80ce676f8429e57a5
= <adniyayousaf92@gmail.com>
2023-11-17 14:45:41 +0000

    Applied the feedback and fix the bugs.

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -2,17 +2,19 @@ let list = document.getElementById("todo-list");
 const addTodo = document.querySelector("#add");
 const todos = [];
 
+
+console.log(todos);
 function addNewTodo() {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   const input = document.querySelector('input');
-  if (input.value != "") {
+  if (input.value !== "") {
     const li = document.createElement("li");
     list.appendChild(li);
-    li.innerHTML = input.value;
+    li.textContent = input.value;
     li.classList.add('list-item');
     const span = document.createElement("span");
-    const htmlContent = '<i id="check" onclick="this.parentNode.parentNode.style.textDecoration=`line-through`;" ></i>'
+    const htmlContent = 
+    '<i id="check" onclick="this.parentNode.parentNode.classList.toggle(`completed`)"></i>'
       + '<i id="trash" onclick="this.parentNode.parentNode.remove()"></i>';
     span.innerHTML = htmlContent;
     li.appendChild(span);
@@ -20,14 +22,17 @@ function addNewTodo() {
   }
 }
 
-populateTodoList(list);
 
-// Advanced challenge: Write a function that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
+// deletes the completed ones
 function deleteAllCompletedTodos() {
-  // Write your code here...
   for (const item of todos) {
-    if (item.getAttribute("style") != null) {
+    if (item.classList.contains('completed')) {
       item.remove();
     }
   }
 }
+
+
+
+
+
