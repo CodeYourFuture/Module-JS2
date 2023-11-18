2bf081f9db03d5d067b651775b50fa204660d9e9
aishaathmanlali <aishaathmanal@gmail.com>
2023-11-18 16:46:18 +0000

    create a readinglist app

--- week-3/reading-list/script.js
+++ week-3/reading-list/script.js
@@ -21,3 +21,27 @@ const books = [
   },
 ];
 
+const readingList = document.getElementById("reading-list");
+for (let index = 0; index < books.length; index++) {
+  const list = document.createElement("li");
+  
+  if(books[index].alreadyRead === true){
+    list.setAttribute("class", "green");
+  }
+  else{
+    list.setAttribute("class", "red");
+  }
+  
+
+  const p = document.createElement("p");
+
+  p.innerText = `${books[index].title} by ${books[index].author}`;
+
+  const div = document.querySelector("#content");
+  readingList.appendChild(list);
+  list.appendChild(p);
+
+  const image = document.createElement("img");
+  image.src = books[index].bookCoverImage;
+  list.appendChild(image);
+}
