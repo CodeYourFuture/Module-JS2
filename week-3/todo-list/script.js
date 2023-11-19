517d85453e45a6288ccdaa583ce8d30aaa72019e
= <adniyayousaf92@gmail.com>
2023-11-19 21:25:32 +0000

    fix

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -54,33 +54,3 @@ function deleteAllCompletedTodos() {
     }
   }
 }
-// // let list = document.getElementById("todo-list");
-// // const addTodo = document.querySelector("#add");
-// // const todos = [];
-
-
-// // console.log(todos);
-// // function addNewTodo() {
-// //   event.preventDefault();
-// //   const input = document.querySelector('input');
-// //   if (input.value !== "") {
-// //     const li = document.createElement("li");
-// //     list.appendChild(li);
-// //     li.textContent = input.value;
-// //     li.classList.add('list-item');
-// //     const span = document.createElement("span");
-// //     const htmlContent = 
-// //     '<i id="check" onclick="this.parentNode.parentNode.classList.toggle(`completed`)"></i>'
-// //       + '<i id="trash" onclick="this.parentNode.parentNode.remove()"></i>';
-// //     span.innerHTML = htmlContent;
-// //     li.appendChild(span);
-// //     todos.push(li);
-// //   }
-// // }
-
-
-
-
-
-
-
