5f18d249432b84db9e43a647b170fcb331bfb8ef
= <adniyayousaf92@gmail.com>
2023-11-20 12:41:20 +0000

    updated the deeleteAllCompled. Intended the code

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -5,32 +5,38 @@ function populateTodoList(todo) {
     const li = document.createElement('li');
     list.appendChild(li);
     li.textContent = item.task;
+
     //add the check button
     const span = document.createElement('span');
     li.appendChild(span);
+
     //Add Deadline
     const date = document.createElement('i');
     span.appendChild(date);
     date.textContent = item.deadline;
+
     const iCheck = document.createElement('i');
     span.appendChild(iCheck);
     iCheck.classList.add('check');
     iCheck.addEventListener("click", () => {
-      li.classList.toggle('completed')
+      item.completed = !item.completed;
+      populateTodoList(todos);
     });
+
     // add the delete button
     const iDelete = document.createElement('i');
     span.appendChild(iDelete);
     iDelete.classList.add('trash');
     iDelete.addEventListener("click", () => {
-      li.remove();
+      todos = todos.filter(todoItem => todoItem !== item);
+      populateTodoList(todos);
     });
   }
 }
+
 const addTodoButton = document.querySelector("#add");
 const input = document.querySelector('#input');
-const date = document.querySelector('#date');
-
+const dateInput = document.querySelector('#date');
 let todos = [
   { task: "Wash the dishes", completed: false, deadline: '2-10-2023' },
   { task: "Do the shopping", completed: false, deadline: '5-11-2023' }
@@ -39,29 +45,25 @@ populateTodoList(todos);
 
 addTodoButton.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log(date.value);
-  const input = document.querySelector('#input');
-  if (input.value !== '' && date.value !== '') {
-    todos.push({
+  console.log(dateInput.value);
+  if (input.value !== '' && dateInput.value !== '') {
+    const newTodo = {
       task: input.value,
       completed: false,
-      deadline: date.value,
-    });
-  } else (
-    alert('Either task or date is empty.')
-  )
+      deadline: dateInput.value,
+    };
+    todos.push(newTodo);
+    populateTodoList(todos);
+  } else {
+    alert('Either task or date is empty.');
+  }
 
-  populateTodoList(todos);
   input.value = '';
-  date.value = '';
-})
+  dateInput.value = '';
+});
 
 // deletes the completed ones
 function deleteAllCompletedTodos() {
-  let listItems = document.getElementsByTagName('li')
-  for (const item of listItems) {
-    if (item.classList.contains('completed')) {
-      item.remove();
-    }
-  }
+  todos = todos.filter(item => !item.completed);
+  populateTodoList(todos);
 }
