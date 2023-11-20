63d71fe518242ea4cd328aed1a39ed90bbdff3ed
= <adniyayousaf92@gmail.com>
2023-11-20 23:27:37 +0000

    updated alarm clock js

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+const timeRemaining = document.querySelector('#timeRemaining');
+let time;
+let input;
+
+function setAlarm() {
+  input = parseInt(document.querySelector('#alarmSet').value, 10);
+  displayTime();
+  time = setInterval(interval, 1000);
+}
+
+function interval() {
+  if (input > 0) {
+    input--;
+    displayTime();
+  } else {
+    clearInterval(time);
+    playAlarm();
+    document.querySelector('.centre').style.background = 'red';
+  }
+}
+
+function displayTime() {
+  const minutes = Math.floor(input / 60);
+  const seconds = input % 60;
+  const formattedTime = `${minutes}:${seconds}`;
+  timeRemaining.textContent ="Time Remaining:" +formattedTime;
+}
+
+function stopAlarm() {
+  clearInterval(time);
+  timeRemaining.textContent = "Time Remaining:00:00";
+  pauseAlarm();
+}
 
 // DO NOT EDIT BELOW HERE
 
