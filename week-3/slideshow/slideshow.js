b5d759ce669c9058f8c50d63ff7cdb4207ef67be
aishaathmanlali <aishaathmanal@gmail.com>
2023-11-26 17:17:41 +0000

    add images and buttons

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -1,8 +1,34 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/penguin1.jpeg",
+  "./assets/penguin2.jpeg",
+  "./assets/penguin4.jpeg",
+  "./assets/penguin5.jpeg",
+  "./assets/penguin6.avif",
+  "./assets/penguin7.jpeg",
 ];
 
 
 // Write your code here
+const picDiv = document.getElementById("slideshow");
+const image = document.createElement("img");
+picDiv.appendChild(image);
+
+let index = 0;
+function displayImages() {
+    image.src = images[index];
+}
+displayImages(index);
+
+
+const nextButton = document.getElementById("forward-btn");
+const previousButton = document.getElementById("backward-btn");
+
+nextButton.addEventListener("click", function() {
+    index = index + 1;
+    displayImages(index);
+});
+
+previousButton.addEventListener("click", function () {
+  index = index - 1;
+  displayImages(index);
+});
