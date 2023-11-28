c234b011911262de6e5fa66cfa7894e58cfb8f2e
AbdihamidAli <abdi747573@gmail.com>
2023-11-28 10:26:16 +0000

    JS2_WEEK2_implement_tasks_with_jest_tests

--- week-2/implement/lookup.js
+++ week-2/implement/lookup.js
@@ -1,10 +1,23 @@
-function createLookup() {
+function createLookup(countryCurrencyPairs) {
   // implementation here
+  // empty object to store the lookup of country, Currency Pairs
+  const lookup = {};
+
+  // Iterate through each country-currency pair in the array
+  for (const pairs of countryCurrencyPairs) {
+    const [countryCode, currencyCode] = pairs;
+
+    // store the values of the currency code to the country code in the lookup object
+    lookup[countryCode] = currencyCode;
+  }
+
+  return lookup;
 }
+console.log(createLookup([["US", "USD"],["CA", "CAD"]]));
 
 /* ======= Test suite is provided below... =====
  */
-
+/*
 test("converts a single pair of currency codes", () => {
   expect(createLookup([["GB", "GBP"]])).toEqual({
     GB: "GBP",
@@ -15,3 +28,5 @@ test("converts a single pair of currency codes", () => {
 });
 
 test.todo("creates a country currency code lookup for multiple codes");
+*/
+module.exports = createLookup;
