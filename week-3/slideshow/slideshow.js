5435991c13c7a002e0df9e82b08ee4cb83385ef7
Marcus <marcus.ats94@gmail.com>
2023-11-30 20:11:23 +0000

    Completed the slideshow exercise

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -1,8 +1,31 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
-
 // Write your code here
+
+const forwardSlide = document.querySelector("#forward-btn");
+const backwardSlide = document.querySelector("#backward-btn");
+const imageSelector = document.querySelector("#carousel-img");
+
+let num = 0;
+imageSelector.style.width = "600px";
+imageSelector.style.height = "400px";
+
+forwardSlide.addEventListener("click", function () {
+  num++;
+  if (num === images.length) {
+    num = 0;
+  }
+  imageSelector.src = images[num];
+});
+
+backwardSlide.addEventListener("click", function () {
+  num--;
+  if (num < 0) {
+    num = images.length - 1;
+  }
+  imageSelector.src = images[num];
+});
