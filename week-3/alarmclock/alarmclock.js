dd0c38b03d6bec828e9e55be39c4bf50a14651c9
aishaathmanlali <aishaathmanal@gmail.com>
2023-12-01 01:16:38 +0000

    create alarm clock app

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,4 +1,50 @@
-function setAlarm() {}
+let timeRemaining = document.getElementById("timeRemaining");
+
+function setAlarm() {
+  let input = document.getElementById("alarmSet").value;
+
+  if (input <= 0) {
+    alert("Invalid number");
+  }
+  else if(input > 0){
+    displayTime(input);
+    const interval = setInterval(() => {
+      if (input === 0) {
+        clearInterval(interval);
+        playAlarm();
+      }
+      displayTime(input--);
+    }, 1000);
+  }
+  console.log(input);
+}
+
+function displayTime(num) {
+ let time;
+ let minutes = Math.floor(num / 60);
+ let seconds = num % 60;
+ 
+ if(num >= 0){
+     
+  if (minutes < 10) {
+    minutesPart = `0${minutes}`;
+  } else {
+    minutesPart = minutes;
+  }
+  if (seconds < 10) {
+    secondsPart = `0${seconds}`;
+  } else {
+    secondsPart = seconds;
+  }
+  time = `${minutesPart}:${secondsPart}`;
+  timeRemaining.textContent = `Time Remaining: ${time}`;
+
+  }
+  else if (input < 0) {
+    time = `00:00`;
+    timeRemaining.textContent = `Time Remaining: ${time}`;
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
