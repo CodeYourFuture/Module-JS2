619a42bfdae635651e9601df5518a0d5bcd0a065
Pedro <riccip@outlook.com>
2023-12-02 16:38:59 +0000

    refactor/find.js done

--- week-1/refactor/find.js
+++ week-1/refactor/find.js
@@ -1,13 +1,19 @@
 // Refactor the implementation of find to use a for...of loop
 
 function find(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
-      return index;
+  for (const e of list) {
+    if (e === target) {
+      return list.indexOf(e)
     }
-  }
-  return -1;
+  } return -1
+
+  /*   for (let index = 0; index < list.length; index++) {
+      const element = list[index];
+      if (element === target) {
+        return index;
+      }
+    }
+    return -1; */
 }
 
 module.exports = find;
