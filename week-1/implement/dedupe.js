adf0fbe4f0cda2eb4bf040d57c3e0f94940888b8
Pedro <riccip@outlook.com>
2023-12-02 15:59:57 +0000

    implement/dedupe done

--- week-1/implement/dedupe.js
+++ week-1/implement/dedupe.js
@@ -1,1 +1,16 @@
-function dedupe() {}
+function dedupe(array) {
+  let deduplicatedArray = []
+
+  if (array.length === 0) {
+    return deduplicatedArray
+  }
+
+  array.forEach(element => {
+    !deduplicatedArray.includes(element) ? deduplicatedArray.push(element) : null
+  });
+  
+  return deduplicatedArray
+}
+
+
+module.exports = dedupe;
