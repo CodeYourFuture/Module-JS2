df5b0572b1603e2b1794e4b90cb5d6d2bf1abda0
Elmira Zangeneh <elmira.zangeneh89@gmail.com>
2023-12-06 21:11:49 +0000

    add button

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -1,6 +1,7 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  list.innerHTML = "";
   todos.forEach((item) => {
     const listItem = document.createElement("li");
     list.appendChild(listItem);
@@ -31,16 +32,30 @@ let todos = [
 populateTodoList(todos);
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 const addButton = document.getElementById("add-button");
-addButton.addEventListener("click", addNewTodo());
+addButton.addEventListener("click", addNewTodo);
 
-function addNewTodo() {
+function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  // event.preventDefault();
+  event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
-  const input = document.getElementById("input-todo").value;
-  console.log(input);
+  const input = document.getElementById("input-todo");
+  if (input.value.length > 0) {
+    const newToDo = {
+      task: input.value,
+      completed: false,
+    };
+
+    todos.push(newToDo);
+    input.value = "";
+    populateTodoList(todos);
+  }
+
+  // console.log(todos);
 }
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
 }
+document.getElementById("date").min = new Date().toISOString().split("T")[0];
+// minTime.min = new date();
+function setDate() {}
