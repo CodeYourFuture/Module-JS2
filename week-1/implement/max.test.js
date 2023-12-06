0c6e86a6c83d48fe71d4e31098e3251a24f3d71f
Fathi Kahin <fhkahin@gmail.com>
2023-12-06 12:32:01 +0000

    max.test.js updated

--- week-1/implement/max.test.js
+++ week-1/implement/max.test.js
@@ -1,3 +1,13 @@
+
+const findMaxNumber = require('./max');
+// Assuming both files are in the same directory
+
+// Your test cases go here
+ 
+// Adjust the path accordingly if your files are in different directories
+
+// Your test cases go here
+
 /* Find the maximum element of an array of numbers
 
 In this kata, you will need to implement a function that sums the numerical elements of an array
@@ -9,7 +19,7 @@ E.g. max(['hey', 10, 'hi', 60, 10]), target output: 60 (sum ignores any non-nume
 // Given an empty array
 // When passed to the max function
 // Then it should return -Infinity
-test.todo("given an empty array, returns -Infinity");
+//test.todo("given an empty array, returns -Infinity");
 
 // Given an array with one number
 // When passed to the max function
@@ -26,3 +36,31 @@ test.todo("given an empty array, returns -Infinity");
 // Given an array with non-number values
 // When passed to the max function
 // Then it should return the max and ignore non-numeric values
+
+//......................................................................
+//ANSWER
+
+test("given an empty array, returns -Infinity", () => {
+  const result = findMaxNumber([]);
+  expect(result).toBe(-Infinity);
+});
+
+test("given an array with one number, returns that number", () => {
+  const result = findMaxNumber([42]);
+  expect(result).toBe(42);
+});
+
+test("given an array with both positive and negative numbers, returns the largest number overall", () => {
+  const result = findMaxNumber([10, -5, 7, -20, 15]);
+  expect(result).toBe(15);
+});
+
+test("given an array with decimal numbers, returns the largest decimal number", () => {
+  const result = findMaxNumber([1.5, 3.7, 2.8, 1.2]);
+  expect(result).toBe(3.7);
+});
+
+test("given an array with non-number values, returns the max and ignores non-numeric values", () => {
+  const result = findMaxNumber(['hey', 10, 'hi', 60, 10]);
+  expect(result).toBe(60);
+});
