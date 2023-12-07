8d91b6ef2f48867c6d942b90267253a184662ff2
haythem-f <haythem00@gmail.com>
2023-12-07 01:47:22 +0000

    w1-imp-dedupe

--- week-1/implement/dedupe.js
+++ week-1/implement/dedupe.js
@@ -1,1 +1,7 @@
-function dedupe() {}
+let arr = [1, 2, 2, 3,"a","a","b","c"];
+function dedupe(arr) {
+    return arr.filter((item,
+        index) => arr.indexOf(item) === index);
+}
+
+console.log(dedupe(arr));
