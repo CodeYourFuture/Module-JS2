b9cd1e0e2268fb3f51dc560be6efcc5b3e760686
LauraSantiag0 <eusoulaura8@gmail.com>
2023-12-08 10:48:29 +0000

    reading

--- week-3/reading-list/script.js
+++ week-3/reading-list/script.js
@@ -1,4 +1,5 @@
-// for the tests, do not modify this array of books
+// for the tests, do not modify this array of book
+
 const books = [
   {
     title: "The Design of Everyday Things",
@@ -9,7 +10,7 @@ const books = [
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true,
+    alreadyRead: false,
     bookCoverImage:
       "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
   },
@@ -20,4 +21,54 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
+
+
+// const contentDiv = document.getElementById("content")
+// const readingList = document.getElementById("reading-list");
+// console.log(readingList)
+// const list = document.createElement("li")
+
+// let p = document.createElement("p");
+// p.innerText = books[0].title
+
+
+
+// const book = document.createElement("li")
+// const details = document.createElement("p")
+
+
+//--------------------------------------------------------------
+
+const readingList = document.getElementById("reading-list")
+
+
+for (const book of books) {
+  
+  const list = document.createElement("li")
+
+  if (book.alreadyRead === true){
+    list.setAttribute("class","green")
+  }
+  else{
+    list.setAttribute("class","red")
+  }
+
+  
+
+  const div = document.getElementById("content")
+
+  readingList.appendChild(list)
+  const p = document.createElement("p")
+  p.innerText = `${book.title} by ${book.author}`
+ 
+  list.appendChild(p);
+
+  const image = document.createElement("img")
+  image.src = book.bookCoverImage;
+  list.appendChild(image);
+}
+
+
+
+
 
