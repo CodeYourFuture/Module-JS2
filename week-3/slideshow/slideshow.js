83d3dac72a9532a791d5113cba706d62c7c08aa1
bahador <noor.bahadory786@gmail.com>
2023-12-08 02:52:03 +0000

    added forward & backward buttton

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -1,8 +1,29 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
-
 // Write your code here
+const forwardBtn = document.querySelector("#forward-btn");
+const backwardBtn = document.querySelector("#backward-btn");
+const carouselImg = document.querySelector("#carousel-img");
+
+let num = 0;
+forwardBtn.addEventListener("click", function () {
+  if (num === images.length - 1) {
+    num = 0;
+  } else {
+    num++;
+  }
+  carouselImg.src = images[num];
+});
+
+backwardBtn.addEventListener("click", function () {
+  if (num === 0) {
+    num == images.length - 1;
+  } else {
+    num--;
+  }
+  carouselImg.src = images[num];
+});
