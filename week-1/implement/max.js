6be97e57cca3b6bca9deccffede2f8a52a76ebe9
Pedro <riccip@outlook.com>
2023-12-08 09:25:22 +0000

    approach changed to filter and reduce methods, as switch is not ideal for this situation

--- week-1/implement/max.js
+++ week-1/implement/max.js
@@ -1,16 +1,9 @@
 function max(array) {
-  let tempArray
-  switch (true) {
-    case array.length === 0:
-      return "-Infinity"
-    case array.length === 1 && !isNaN(array[0]):
-      return array[0]
-    case array.length > 1:
-      tempArray = array.filter((e) => !isNaN(e))
-      return Math.max(...tempArray)
-    default:
-      return "Invalid Array"
+  if (array && array.length > 0) {
+    let maxValue = array.filter((value) => typeof value === 'number').reduce((a, b) => a > b ? a : b);
+    return maxValue;
   }
+  return "-Infinity"
 }
 
 module.exports = max;
