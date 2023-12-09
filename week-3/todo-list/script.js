65c6a9acc7ab9a2eabfc9a0b2af1771d1972f22a
aishaathmanlali <aishaathmanal@gmail.com>
2023-12-09 16:24:57 +0000

    correction

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -13,50 +13,52 @@ function populateTodoList() {
   let input = document.querySelector("input").value;
    todoList.innerHTML = "";
    for (const item of todos) {
-  let listItem = document.createElement("li");
-  let checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox");
-   checkbox.setAttribute("id", todos.indexOf(item));
-  let listItemLabel = document.createElement("label");
-  listItemLabel.setAttribute("for", todos.indexOf(item));
-   listItemLabel.innerText = item.task;
-  listItem.appendChild(listItemLabel);
-  listItem.appendChild(checkbox);
+     let listItem = document.createElement("li");
+     let checkbox = document.createElement("input");
+     checkbox.setAttribute("type", "checkbox");
+     checkbox.setAttribute("id", todos.indexOf(item));
+     let listItemLabel = document.createElement("label");
+     listItemLabel.setAttribute("for", todos.indexOf(item));
+     listItemLabel.innerText = item.task;
+     listItem.appendChild(listItemLabel);
+     listItem.appendChild(checkbox);
 
-  // Check box line strike
-  checkbox.addEventListener("change", function (event) {
-    if (event.target.checked) {
-      listItem.setAttribute("class", "strike");
-    } else {
-      listItem.setAttribute("class", "no-strike");
-    }
-  });
+     // Check box line strike
+     checkbox.addEventListener("change", function (event) {
+       if (event.target.checked) {
+         listItem.setAttribute("class", "strike");
+       } else {
+         listItem.setAttribute("class", "no-strike");
+       }
+     });
 
-  //  //trash button
-  let trashSpan = document.createElement("span");
-  listItem.appendChild(trashSpan);
-  let trashIcon = document.createElement("i");
-  trashIcon.setAttribute("class", "fa fa-trash-o");
-  trashSpan.appendChild(trashIcon);
+     //  //trash button
+     let trashSpan = document.createElement("span");
+     listItem.appendChild(trashSpan);
+     let trashIcon = document.createElement("i");
+     trashIcon.setAttribute("class", "fa fa-trash-o");
+     trashSpan.appendChild(trashIcon);
 
-  //  //delete task event
+     //  //delete task event
 
-  trashSpan.addEventListener("click", () => {
-    let index = todos.indexOf(item);
-    if (index > -1) {
-      todos.splice(index, 1);
-    }
-    populateTodoList(); //refresh the todo list
-  });
+     trashSpan.addEventListener("click", () => {
+       let index = todos.indexOf(item);
+       if (index > -1) {
+         todos.splice(index, 1);
+       }
+       populateTodoList(); //refresh the todo list
+     });
 
-  removeAllButton.addEventListener("click", () => {
-    document.querySelector(".strike").remove(); 
-  });
+     //remove all button
+     removeAllButton.addEventListener("click", () => {
+       document.querySelector(".strike").remove();
+      //  populateTodoList();
+     });
 
-  todoList.appendChild(listItem);
+     todoList.appendChild(listItem);
+   }
 }
-}
-
+ 
 
 
 
