e203a3ec80fdc09dcd6eb310f5afbf19988a8478
AbdihamidAli <abdi747573@gmail.com>
2023-12-09 03:58:33 +0000

    JS2_WEEK3_toDoList_task

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -1,25 +1,107 @@
-function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const todoForm = document.getElementById("todo-form");
+  const todoInput = document.getElementById("todoInput");
+  const deadlineInput = document.getElementById("deadlineInput");
+  const todoList = document.getElementById("todo-list");
+  const removeAllCompletedButton = document.getElementById(
+    "remove-all-completed"
+  );
 
-// These are the same todos that currently display in the HTML
-// You will want to remove the ones in the current HTML after you have created them using JavaScript
-let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
-];
+  let todos = [
+    { task: "Wash the dishes", completed: false, deadline: "" },
+    { task: "Do the shopping", completed: false, deadline: "" },
+  ];
 
-populateTodoList(todos);
+  function populateTodoList() {
+    todoList.innerHTML = "";
 
-// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
-function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
-}
+    todos.forEach((todo, index) => {
+      const li = document.createElement("li");
+      const checkbox = document.createElement("input");
+      const label = document.createElement("label");
+      const deadlineText = document.createElement("span");
+      const deleteIcon = document.createElement("span");
 
-// Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
-  // Write your code here...
-}
+      checkbox.type = "checkbox";
+      checkbox.checked = todo.completed;
+      checkbox.addEventListener("change", () => toggleTodoCompleted(index));
+
+      label.textContent = todo.task;
+      label.style.textDecoration = todo.completed ? "line-through" : "none";
+
+      // Display deadline if set
+      if (todo.deadline) {
+        deadlineText.textContent = ` | ${formatDeadline(
+          new Date(todo.deadline)
+        )}`;
+      }
+
+      deleteIcon.innerHTML = "&#x1F5D1;";
+      deleteIcon.style.cursor = "pointer";
+      deleteIcon.addEventListener("click", () => {
+        if (checkbox.checked) {
+          deleteTodo(index);
+        }
+      });
+
+      li.appendChild(checkbox);
+      li.appendChild(label);
+      li.appendChild(deadlineText);
+      li.appendChild(deleteIcon);
+
+      // Add custom styling for checked checkboxes
+      if (checkbox.checked) {
+        checkbox.style.backgroundColor = "green";
+
+        checkbox.parentNode.style.color = "green";
+      }
+
+      todoList.appendChild(li);
+    });
+  }
+
+  function addNewTodo(event) {
+    event.preventDefault();
+
+    const newTodoText = todoInput.value.trim();
+    const newTodoDeadline = deadlineInput.value.trim();
+
+    if (newTodoText !== "") {
+      todos.push({
+        task: newTodoText,
+        completed: false,
+        deadline: newTodoDeadline,
+      });
+      todoInput.value = "";
+      deadlineInput.value = "";
+      populateTodoList();
+    }
+  }
+
+  function toggleTodoCompleted(index) {
+    todos[index].completed = !todos[index].completed;
+    populateTodoList();
+  }
+
+  function deleteTodo(index) {
+    todos.splice(index, 1);
+    populateTodoList();
+  }
+
+  function deleteAllCompletedTodos() {
+    todos = todos.filter((todo) => !todo.completed);
+    populateTodoList();
+  }
+
+  function formatDeadline(deadline) {
+    // Customize the formatting of the deadline (e.g., using libraries like moment.js)
+    return deadline.toLocaleString();
+  }
+
+  // Event listeners
+  todoForm.addEventListener("submit", addNewTodo);
+  removeAllCompletedButton.addEventListener("click", deleteAllCompletedTodos);
+
+  // Initial population of the todo list
+  populateTodoList();
+});
