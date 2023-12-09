6cc14f88b5a62b01c05e8f5359656c103199ef6a
RbAvci <rb.avci@gmail.com>
2023-12-09 00:09:08 +0000

    fixed refactor

--- week-1/refactor/find.js
+++ week-1/refactor/find.js
@@ -1,7 +1,6 @@
 // Refactor the implementation of find to use a for...of loop
 
 function find(list, target) {
-  let index = 0;
   for (const element of list){
     if (element === target) {
       return list.indexOf(element);
