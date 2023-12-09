5dc96c529ada962be0bf56d67d08dce18312511b
karimi <behrouzkarimi1993@gmail.com>
2023-12-09 22:08:40 +0000

    make it tidy

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -144,98 +144,42 @@ removeAllCompletedButton.addEventListener('click',deleteAllCompletedTodos);
 
 /////////////// date setup ///////////////////////////////
 
-// initialising deadline
-// function initialDeadLine(deadLineProperty){
-//     // get deadLine date , creating a new date object based on deadlineproperty
-//     const deadLineTime=new Date(deadLineProperty);
-
-//     // declare variable of current date to colculate remained date
-//     const currentTime=new Date();
-
-//     //calculate diffrence time by Miliseconds
-//     const diffTime=deadLineTime-currentTime;
-    
-//     //change it into days
-//     const diffTimeByDays=Math.ceil(diffTime/(1000*60*60*24));
-//     console.log('this is diffdays',diffTimeByDays);
-
-//     return diffTimeByDays;
-// }
-
-// deadline calculator takes the todo deadlineproperty from the object
-// and returns different time between today and deadline todo date by days
-
-// function updateDeadLineCalculator(initialDeadLine,deadLineProperty){
-//   let daysForInterval=(24*3600*1000);
-//   const numberOfDays=initialDeadLine(deadLineProperty);
-//   const intervalDeadLine=setTimeout(() => {
-//     if(numberOfDays===''){
-//     return 'Date is not set';
-
-//     }
-
-//   if(numberOfDays<=0){
-//     clearInterval(intervalDeadLine);
-//     }
-//   }, daysForInterval);
-  
-  
-// }
-
 function updateDeadLineCalculator(deadLineProperty) {
   const daysForInterval = (24 * 3600 * 1000);
-  const currentDate=new Date();
-  const deadLineDate=new Date(deadLineProperty);
+  const currentDate=new Date(); // get current date
+  const deadLineDate=new Date(deadLineProperty); // get the date task needs to be done
 
-  const differenceOfDays=Math.ceil((deadLineDate-currentDate)/daysForInterval);
+  // subtract and calculate the diiference  base on days
+  const differenceOfDays=Math.ceil((deadLineDate-currentDate)/daysForInterval); // difference is miliSec
   const year=Math.floor(differenceOfDays/365);
   const month=Math.floor(differenceOfDays% 365 / 30);
   const days=Math.floor(differenceOfDays%365%30);
 
   let content='';
+  // just shows days as other are 0
   if(deadLineProperty.length!=0){
-  if(days>0 && month===0 && year===0){
-    content= `${days} day's left`;
-  }
-  else if(days>0 && month>0 && year===0){
-    content =`${month} months  ${days} day's left!`;
-  }
+    if(days>0 && month===0 && year===0){
+      content= `${days} day's left`;
+    }
+    else if(days>0 && month>0 && year===0){
+      content =`${month} months  ${days} day's left!`;
+    }
 
-  else if(days===0 && month===0&& year===0){
-    content = 'few hours left!';
-  }
-  else {
-    content=`${year} years and ${month} months and ${days} day's left! `;
-  }
-  
-  return content;
-  }
+    else if(days===0 && month===0&& year===0){
+      content = 'few hours left!';
+    }
+    else {
+      content=`${year} years and ${month} months and ${days} day's left! `;
+    }
+    
+    return content;
+    }
 
   else{
     return 'Date not been set';
   }
 
 }
-
-
-
-// set the date value for the sapn tag which  holds date in list
-// setDateValue function prepare the text for the sapn tag which shows the remained time for the todo by days
-
-// function setDateValue(numberOfDays){
-//   //calcualting remained years for todo task
-//   const years=Math.floor(numberOfDays/365);
-//   //calculating remained month for todo task
-//   const months=Math.floor(numberOfDays%365/30);
-//   //calculating remained days for todo task
-//   const days=Math.ceil(numberOfDays%365%30);
-
-//   // based on the remained days proper message will be returned
-
-//   //let message='';
-  
-
-// }
 
 
 // set up back ground color of deadline date
