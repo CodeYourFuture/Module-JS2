3b69df5b62e6a9e0b5fdacefe1cdde14606c13d3
RbAvci <rb.avci@gmail.com>
2023-12-09 00:08:28 +0000

    fixed refactor

--- week-1/refactor/find.js
+++ week-1/refactor/find.js
@@ -4,9 +4,8 @@ function find(list, target) {
   let index = 0;
   for (const element of list){
     if (element === target) {
-      return index;
+      return list.indexOf(element);
     }
-    index++;
   }
   return -1;
 }
