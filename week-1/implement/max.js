9ff514dcff0be57c26537f0354572111aa041cf2
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-10 21:15:52 +0000

    added if condition to check if it is not a number

--- week-1/implement/max.js
+++ week-1/implement/max.js
@@ -1,32 +1,23 @@
 function max(maxlist) {
     let arr = [];
     
-    
     for (let i = 0; i < maxlist.length; i++) {
       if (typeof maxlist[i] === "number") {
         arr.push(maxlist[i]);
       } 
       else if (typeof maxlist[i] === "string") {
-        // Attempt to convert the string to a number
+        //parseInt mean to convert the string to a number.
         const numberValue = parseInt(maxlist[i]);
-  
-        // Check if the conversion was successful and the result is a number
-        if (!isNaN(numberValue)) {
-          arr.push(numberValue);
-         }
       }
     }
     if(arr.length === 0){
         return -Infinity;
          }
-    
-  
-    //return arr;
-    const maxString= Math.max(...arr);
+    const maxString= Math.max.apply(null, arr);
     return maxString;
     // return Math.min.apply(null, result);
   }
   
   module.exports = max;
-  //console.log(sum([2.45,'err' ,3.87, -40,]));
+  //console.log(max([2.45,'err' ,3.87, -40,]));
 
