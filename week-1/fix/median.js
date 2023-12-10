fe8b038f4356f07f64de5dbe1e5d2fc27edbccde
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-10 19:54:29 +0000

    added code

--- week-1/fix/median.js
+++ week-1/fix/median.js
@@ -2,15 +2,22 @@
 //Start by running the tests for this function
 //If you're in the week-1 directory, you can run npm test -- fix to run the tests in the fix directory
 
-function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+ function calculateMedian(list) {
+  const listsToSort= list.sort((a, b) => a - b);
+  const middleIndex = Math.floor(listsToSort.length / 2);
+  const elementsInMiddle = listsToSort.slice(middleIndex - 1, middleIndex + 1);
+  
+  for (let i = 0; i < listsToSort.length; i++){
+    if(listsToSort.length % 2 === 0){
+    return (elementsInMiddle[0] + elementsInMiddle[1]) / 2;
+ }
+ else {
+    return listsToSort[middleIndex];
+  }
 }
 
-//calculateMedian( 3, 3, 4, 5)
-//module.exports = calculateMedian;
+};
 
-// console.log(calculateMedian( 3, 3, 4, 5))
+module.exports = calculateMedian;
 
 
