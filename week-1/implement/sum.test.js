150e49cfa6f935174d5c12db03a44cdf6c950624
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-10 21:21:54 +0000

    written some code and also included a new test case.

--- week-1/implement/sum.test.js
+++ week-1/implement/sum.test.js
@@ -12,18 +12,38 @@ E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical
 // When passed to the sum function
 // Then it should return 0
 
+const sum = require("./sum");
+test("given an empty array, Then it should return 0", () => {
+  expect(sum([])).toEqual(0);
+});
+
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+
+test("given array with just one number, Then it should that number", () => {
+    expect(sum([2])).toEqual(2);
+  });
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
 
+test("given array containing negative numbers, return the correct total sum", () => {
+    expect(sum([2, 4, -6, 5])).toEqual(5);
+  });
+
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test("given array  with decimal/float numbers, return the correct total sum", () => {
+    expect(sum([34, 3.5, 5, 0.2])).toEqual(42.7);
+  });
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+
+test("given array  with decimal/float numbers, return the correct total sum", () => {
+    expect(sum([5, "er", 5, 0.2])).toEqual(10.2);
+  });
