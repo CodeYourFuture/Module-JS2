f9080236f2f92713f00d3e4ade01d112189fa57d
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-10 19:50:11 +0000

    added code on test.js file

--- week-1/implement/dedupe.test.js
+++ week-1/implement/dedupe.test.js
@@ -1,4 +1,4 @@
-const dedupe = require("./dedupe.js");
+
 /*
 Dedupe Array
 
@@ -15,12 +15,27 @@ E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) target output: [5, 1, 2, 3, 8]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+
+const dedupe = require("./dedupe");
+
+describe("dedupe", () => {
+    test("given an empty array, it returns an empty array", () => {
+      expect(dedupe([])).toEqual([]);;
+    });
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
 
+test("given array with no duplicates, it return a copy of the original array", () => {
+    expect(dedupe([3, 4])).toEqual([3, 4]);
+  });
+
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values
+
+test("given array with strings or numbers, it should remove the duplicate values", () => {
+    expect(dedupe([3, 3, 6, "er", "er", 4])).toEqual([3, 6,"er", 4]);
+  });
+});
