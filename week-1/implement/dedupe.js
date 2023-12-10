51b2a63cce140adec47090725b2a6389c75a7813
haythem-f <haythem00@gmail.com>
2023-12-10 14:04:25 +0000

    w1-implement

--- week-1/implement/dedupe.js
+++ week-1/implement/dedupe.js
@@ -1,7 +1,6 @@
-let arr = [1, 2, 2, 3,"a","a","b","c"];
+let arr = [1, 2, 2, 3, "a", "a", "b", "c"];
 function dedupe(arr) {
-    return arr.filter((item,
-        index) => arr.indexOf(item) === index);
+  return arr.filter((item, index) => arr.indexOf(item) === index);
 }
 
 console.log(dedupe(arr));
---
+++ week-1/implement/max.js
@@ -0,0 +1,13 @@
+function largest(arr) {
+  let max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      if (arr[i] > max) max = arr[i];
+    }
+  }
+
+  return max;
+}
+let arr = ["tt", 98, -9, 1, 39, "a", "df", 99];
+console.log("Largest in given array is " + largest(arr));
