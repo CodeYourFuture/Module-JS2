c72287dbe9b6861d0ee8fede8a40ef6d4022e1e3
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-12 09:44:47 +0000

    I have removed some commands

--- week-1/stretch/aoc-2018-day1/solution.js
+++ week-1/stretch/aoc-2018-day1/solution.js
@@ -1,28 +1,20 @@
-// function puzzle(stars){
-//     let output = []
-//     const arr = stars.reduce((a, b) => a + b, 0)
-//     for (let i = 0; i < stars.length; i++){
-//         output.push[i];
+const fs = requires('fs');
 
-//     }
-//     return arr;
-// }
+function puzzle(stars){
+    
+    let output = []
+    const arr = stars.reduce((a, b) => a + b, 0)
+    for (let i = 0; i < stars.length; i++){
+        output.push[i];
+
+    }
+    return arr;
+}
 
 
 
 //console.log(puzzle([2,6,-5,-4,9]))
 
-
-const fs = require('fs');
-
-function puzzle(stars) {
-    let output = [];
-    const arr = stars.reduce((a, b) => a + b, 0);
-    for (let i = 0; i < stars.length; i++) {
-        output.push[i];
-    }
-    return arr;
-}
 
 // Read data from input.txt
 
