7093799a29ebf5b7ee9701dd149f2429a41046b6
Pedro <riccip@outlook.com>
2023-12-12 10:37:47 +0000

    debug/address done

--- week-2/debug/address.js
+++ week-2/debug/address.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-
+// The parameter in console.log is using the array notation, not the object notation.
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +12,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
