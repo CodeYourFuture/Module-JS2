fe443a21b6351c9b1ceaef82e47ca1eb1a4cfda8
Fathi Kahin <fhkahin@gmail.com>
2023-12-13 14:09:13 +0000

    contains passed the test

--- week-2/implement/contains.js
+++ week-2/implement/contains.js
@@ -1,3 +1,18 @@
-function contains() {}
+// function contains() {}
 
+
+//.............................................
+
+//ANSWER
+
+function contains(object, propertyName) {
+    // here we are cheching if the object is a valid object (not an array or null)
+    if (object === null || typeof object !== 'object') {
+      return false;
+    }
+  
+    // Here we are Checking if the object has the provided property
+    return object.hasOwnProperty(propertyName);
+  }
+  
 module.exports = contains;
