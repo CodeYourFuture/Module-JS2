6950ded78699f1116826ccece4e9324cb5ad14b0
Fathi Kahin <fhkahin@gmail.com>
2023-12-13 13:07:34 +0000

    NW6 | Fathi_Kahin | JS2  |  week 2 exercises | Week 2

--- week-2/debug/address.js
+++ week-2/debug/address.js
@@ -4,6 +4,21 @@
 // but it isn't working...
 // Fix anything that isn't working
 
+// const address = {
+//   houseNumber: 42,
+//   street: "Imaginary Road",
+//   city: "Manchester",
+//   country: "England",
+//   postcode: "XYZ 123",
+// };
+
+// console.log(`My house number is ${address[0]}`);
+
+//..........................................................
+//ANSWER
+
+// the above code is not working because if this (address[0]). this is incorrect because address is an object, not an array.
+// this is the fixed code:
 const address = {
   houseNumber: 42,
   street: "Imaginary Road",
@@ -12,4 +27,5 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
+
