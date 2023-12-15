6d5fed9cb7989e5326f60aa45f03ca06d9d65c92
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-15 13:04:53 +0000

    I have provided instructions on how to format text into separate lines.

--- week-2/debug/recipe.js
+++ week-2/debug/recipe.js
@@ -1,15 +1,18 @@
 // Predict and explain first...
-
+//Ans; As we did not use any code to iterate the values of the ${recipe} object, it would not give us any output.
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+// Ans: we can fixed it by using dote notation 'recipe.ingredients'. and for ingredient to logged each item on a new line , we have to use for-of loop to iterate each items of ingredient in a new line.
 
 const recipe = {
   title: "bruschetta",
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
-
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+  ingredients:`);
+
+  for (const ingredient of recipe.ingredients) {
+    console.log(` * ${ingredient}`);
+}
