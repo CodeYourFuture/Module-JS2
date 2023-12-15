1b4797f0c636cd690d4e6fc3e024199cbe4ebcfe
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-15 13:09:01 +0000

    added test case

--- week-2/implement/contains.test.js
+++ week-2/implement/contains.test.js
@@ -1,4 +1,4 @@
-const contains = require("./contains.js");
+const contains = require("./contains");
 
 /*
 Implement a function called contains that checks an object contains a
@@ -17,18 +17,46 @@ as the object doesn't contains a key of 'c'
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
 
+test("When passed an object and a property name, it should return true if the object contains the property, false otherwise" , () => {
+    const object = {a: 3, c: 7};
+    const objectTwo = {obj: object, propName: 'c'}
+    expect(contains(objectTwo)).toEqual(true);
+});
+
 // Given an empty object
 // When passed to contains
 // Then it should return false
+
+test("When passed a an empty object, it should return false" , () => {
+    const emptyObject = {};
+    const testEmptyObject = {obj: emptyObject , propName: 'someProp'}
+    expect(contains(testEmptyObject )).toEqual(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
 
+test("When passed to contains with an existing property name then it should return true" , () => {
+    const propObject = {name: 'sonia', age: 10};
+    const testPropObject = {obj: propObject , propName: 'name'}
+    expect(contains(testPropObject)).toEqual(true);
+});
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
 
+test("When passed to contains with a non-existing property name then it should return false" , () => {
+    const propObjectTwo = {name: 'sonia', age: 10};
+    const nonExisting  = {obj: propObjectTwo , propName: 'address'}
+    expect(contains(nonExisting)).toEqual(false);
+});
 // Given invalid parameters like arrays
 // When passed to contains
 // Then it should return false or throw an error
+
+test("When passed to contains like arrays then it should return false or throw an error" , () => {
+    const array= ['string', 34, 89];
+    const arrayNameObj  = {obj: array , propName: ''}
+    expect(contains(arrayNameObj)).toEqual(false);
+});
