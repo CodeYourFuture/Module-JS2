75b72d8a6a6e907be343941517fb0a2b0a0f0313
aishaathmanlali <aishaathmanal@gmail.com>
2023-12-15 17:29:54 +0000

    fix remove all button code to work properly

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -3,72 +3,68 @@ const todoList = document.getElementById("todo-list");
 const removeAllButton = document.getElementById("remove-all-completed");
 
 function addNewTodo() {
-    let input = document.querySelector("input").value;
-    todos[todos.length] = { task: input, completed: false };
-    console.log(todos);
-    populateTodoList();
+  let input = document.querySelector("input").value;
+  todos[todos.length] = { task: input, completed: false };
+  console.log(todos);
+  populateTodoList();
+}
+
+function removeCompleted() {
+  const indexesToExclude = [];
+  for (const item of todos) {
+    if (item.completed) {
+      let index = todos.indexOf(item);
+      indexesToExclude.push(index);
+      console.log(indexesToExclude);
+      document.querySelector(".strike").remove();
+    }
+  }
+  todos = todos.filter((value, index) => !indexesToExclude.includes(index));
 }
 
 function populateTodoList() {
   let input = document.querySelector("input").value;
-   todoList.innerHTML = "";
-   for (const item of todos) {
-     let listItem = document.createElement("li");
-     let checkbox = document.createElement("input");
-     checkbox.setAttribute("type", "checkbox");
-     checkbox.setAttribute("id", todos.indexOf(item));
-     let listItemLabel = document.createElement("label");
-     listItemLabel.setAttribute("for", todos.indexOf(item));
-     listItemLabel.innerText = item.task;
-     listItem.appendChild(listItemLabel);
-     listItem.appendChild(checkbox);
+  todoList.innerHTML = "";
+  for (const item of todos) {
+    let listItem = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("id", todos.indexOf(item));
+    let listItemLabel = document.createElement("label");
+    listItemLabel.setAttribute("for", todos.indexOf(item));
+    listItemLabel.innerText = item.task;
+    listItem.appendChild(listItemLabel);
+    listItem.appendChild(checkbox);
 
-     // Check box line strike
-     checkbox.addEventListener("change", function (event) {
-       if (event.target.checked) {
-         listItem.setAttribute("class", "strike");
-         item.completed = true;
-       } else {
-         listItem.setAttribute("class", "no-strike");
-         item.completed = false;
-       }
-     });
+    // Check box line strike
+    checkbox.addEventListener("change", function (event) {
+      if (event.target.checked) {
+        listItem.setAttribute("class", "strike");
+        item.completed = true;
+      } else {
+        listItem.setAttribute("class", "no-strike");
+        item.completed = false;
+      }
+    });
 
-     //  //trash button
-     let trashSpan = document.createElement("span");
-     listItem.appendChild(trashSpan);
-     let trashIcon = document.createElement("i");
-     trashIcon.setAttribute("class", "fa fa-trash-o");
-     trashSpan.appendChild(trashIcon);
+    //  //trash button
+    let trashSpan = document.createElement("span");
+    listItem.appendChild(trashSpan);
+    let trashIcon = document.createElement("i");
+    trashIcon.setAttribute("class", "fa fa-trash-o");
+    trashSpan.appendChild(trashIcon);
 
-     //  //delete task event
+    //  //delete task event
 
-     trashSpan.addEventListener("click", () => {
-       let index = todos.indexOf(item);
-       if (index > -1) {
-         todos.splice(index, 1);
-       }
-       populateTodoList(); //refresh the todo list
-     });
-
-     function removeCompletedItems() {
-       if (item.completed === true) {
-        let completedItem = item.completed === true;
-         let index = todos.indexOf(completedItem);
-          todos.splice(index);
-         document.querySelector(".strike").remove();
-       }
-     }
-     //remove all button
-     removeAllButton.addEventListener("click", removeCompletedItems);
-
-     todoList.appendChild(listItem);
-   }
+    trashSpan.addEventListener("click", () => {
+      let index = todos.indexOf(item);
+      if (index > -1) {
+        todos.splice(index, 1);
+      }
+      populateTodoList(); //refresh the todo list
+    });
+    todoList.appendChild(listItem);
+  }
 }
- 
-
-
-
 
 // populateTodoList(); // populate Todo List when the page loads
-
