ba0780617e41cd3a7c59b959aa1887dbc1922e86
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-15 12:59:58 +0000

    I have added some codes and comments in response to the questions.

--- week-2/debug/author.js
+++ week-2/debug/author.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
-
+//Ans: You used for- of loop to iterate each properties of object and log out(value) which cause as error because it can iterate value.
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
+// Ans: by default objects are not iterable in java
+// therefore, we need to iterate over the property values
+// "Object.values(author))" - author is an object bring out the values
 
 const author = {
   firstName: "Zadie",
@@ -11,6 +14,9 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
+ 
 }
+
+ //Object.values(author).forEach((value) => console.log(value));
