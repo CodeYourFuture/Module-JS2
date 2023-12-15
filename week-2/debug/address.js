c86d5aac793acb315969ca5eb31d7ea423a77d24
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-15 12:53:32 +0000

    added notation to fix the problem

--- week-2/debug/address.js
+++ week-2/debug/address.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-
+// Ans: the output for [0] will be undefine becasue we incorrectly attempted to use array like notation ${address[0]} on the houseNumber, which is a number. To acces the value of houseNumber propety we should use ${address.houseNumber}.
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +12,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
