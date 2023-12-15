4e2d1f32916387ab4f3d488ac9382f31046c768c
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-15 13:13:49 +0000

    added test case for tally.js

--- week-2/implement/tally.test.js
+++ week-2/implement/tally.test.js
@@ -12,20 +12,39 @@
  * tally(['a','a','b','c']), target output: { a : 2, b: 1, c: 1 }
  */
 
+const tally = require("./tally");
+
 // Acceptance criteria:
 
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
 
+test("When passed an array of items Then it should return an object containing the count for each unique item",() =>{
+
+    expect(tally(['a', 'a', 'a'])).toEqual({a: 3})
+});
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
 
+test("When passed an empty array Then it should return an empty object",() =>{
+
+    expect(tally([])).toEqual({})
+});
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
 
+test("When passed an array with duplicate items Then it should return counts for each unique item",() =>{
+
+    expect(tally(['a', 'a', 'b', 'c'])).toEqual({a: 2, b: 1, c: 1})
+});
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+
+test("When passed an invalid input like a string Then it should throw an error",() =>{
+
+    expect(tally("Working on kata")).toEqual('error')
+});
