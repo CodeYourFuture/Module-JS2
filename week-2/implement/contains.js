6a91a1f5e448ea084f0d5b59fd7f4537ee7b8ef4
Bakhat Begum <bakhtbagum@gmail.com>
2023-12-15 13:08:01 +0000

    I added a loop to determine the appropriate reaction based on the value of propName.

--- week-2/implement/contains.js
+++ week-2/implement/contains.js
@@ -1,3 +1,48 @@
-function contains() {}
+function contains(property) {
+    const obj = property.obj
+    const propName = property.propName;
+   // if(typeof obj !== 'object' || Array.isArray(obj) || obj === null){
+     //   return false;
+    //}
+for (let key in obj) {
+    if (obj.hasOwnProperty(key) && key === propName) {
+      return true;
+  }
+}
+  return false;
+};
+ 
+
+//const invalidInput = {a: 6, b: 8};
+//console.log(contains({ obj: invalidInput,propName: 'b' })); // false
+
+//const object = {a: 3, c: 7}
+//console.log(contains({obj: object, propName: 'b'})) // false
+
+
 
 module.exports = contains;
+
+
+
+
+// let Object = property.Object;
+//     let propName = property.propName;
+//     if(typeof Object !== 'object' || Array.isArray(Object) || Object  === null){
+//         return false;
+//     }
+// for (let key in Object) {
+//     if (Object.hasOwnProperty(key) && key === propName) {
+//       return true;
+//   }
+// }
+//   return false;
+
+
+// for (let prop in objectName) {
+//     if (objectName.hasOwnProperty(prop)) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
