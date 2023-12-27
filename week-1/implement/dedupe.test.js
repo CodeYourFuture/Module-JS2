b90cecb63554d78333b6d3b3c04e7e72a568a5cc
zeliha pala <zelihapala78@gmail.com>
2023-12-27 19:19:20 +0000

    dedupe.js and dedupe.test.js done

--- week-1/implement/dedupe.test.js
+++ week-1/implement/dedupe.test.js
@@ -1,4 +1,3 @@
-const dedupe = require("./dedupe.js");
 /*
 Dedupe Array
 
@@ -11,12 +10,34 @@ E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) target output: [5, 1, 2, 3, 8]
 */
 
 // Acceptance Criteria:
-const calculateMedian = require("./dedupe.js");
+const dedupe = require("./dedupe.js");
+
+//Describe function is used to group together related tests under a common heading or category.
+
+describe("dedupe function", () => {
+  test("given an empty array, it returns an empty array", () => {
+    const emptyArray = [];
+    expect(dedupe(emptyArray)).toEqual([]);
+    //  This test checks if the function returns an empty array when an empty array is provided.
+  });
+
+  test("given an array with no duplicates, it returns a copy of the original array", () => {
+    const noDuplicatesArray = ["a", "b", "c"];
+    expect(dedupe(noDuplicatesArray)).toEqual(noDuplicatesArray);
+    // This test checks if the function returns the original array when there are no duplicates.
+  });
+
+  test("given an array with duplicates, it removes the duplicate values", () => {
+    const arrayWithDuplicates = [5, 1, 1, 2, 3, 2, 5, 8];
+    const expectedOutput = [5, 1, 2, 3, 8];
+    expect(dedupe(arrayWithDuplicates)).toEqual(expectedOutput);
+    //  This test checks if the function removes duplicate values and returns the expected output.
+  });
+});
 
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
 
 // Given an array with no duplicates
 // When passed to the dedupe function
