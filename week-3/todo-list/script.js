091a7a6bfa9778d5479ff0f4aa6b42b61e947dac
Elmira Zangeneh <elmira.zangeneh89@gmail.com>
2023-12-28 00:09:23 +0000

    add checkbox

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -1,41 +1,48 @@
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
   list.innerHTML = "";
+
   todos.forEach((item) => {
     const listItem = document.createElement("li");
     list.appendChild(listItem);
     listItem.textContent = item.task;
     // Create and append a checkbox
-    const checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
+    const checkBox = document.createElement("i");
     listItem.appendChild(checkBox);
+    checkBox.classList.add("check-box");
 
     const span = document.createElement("span");
     listItem.appendChild(span);
     span.classList.add("badge", "bg-primary", "rounded-pill");
-    // const firstI = document.createElement("i");
-    // firstI.classList.add("fa", "fa-check");
-    span.appendChild(firstI);
-    const secondI = document.createElement("i");
-    secondI.classList.add("fa", "fa-trash");
-    span.appendChild(secondI);
-    secondI.addEventListener("click", () => {
+
+    // Create and append a check icon
+    // const checkIcon = document.createElement("i");
+    // checkIcon.classList.add("fa", "fa-check");
+    // span.appendChild(checkIcon);
+
+    const crashIcon = document.createElement("i");
+    crashIcon.classList.add("fa", "fa-trash");
+    span.appendChild(crashIcon);
+
+    crashIcon.addEventListener("click", () => {
       listItem.remove();
       let index = todos.indexOf(item);
       todos.splice(index, 1);
     });
-      // Add a change event listener to the checkbox
-    checkBox.addEventListener("change", () => {
-      listItem.classList.toggle("completed", checkBox.checked);
+
+    // Add a change event listener to the checkbox
+    checkBox.addEventListener("click", () => {
+      listItem.classList.toggle("completed");
+      checkBox.classList.toggle("fa-solid");
+      checkBox.classList.toggle("fa-check");
     });
   });
 }
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
+  // { task: "Wash the dishes", completed: false },
+  // { task: "Do the shopping", completed: false },
 ];
 populateTodoList(todos);
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
@@ -60,9 +67,20 @@ function addNewTodo(event) {
 
   // console.log(todos);
 }
+const deletAll = document.getElementById("remove-all-completed");
+deletAll.addEventListener(
+  "click",
+  deleteAllCompletedTodo
+); /*--> ino khodam neveshtam*/
+
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
+  const incompletedTodos = todos.filter((item) => !item.completed);
+  // Update the todos array
+
+  // Repopulate the todo list
+  populateTodoList(incompletedTodos);
 }
 document.getElementById("date").min = new Date().toISOString().split("T")[0];
 // minTime.min = new date();
