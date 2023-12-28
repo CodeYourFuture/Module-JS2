69b930e59a7ce833a7f4a1c9a782d92398348370
zeliha pala <zelihapala78@gmail.com>
2023-12-28 16:55:21 +0000

    address.js, author.js and recipe.js done

--- week-2/debug/author.js
+++ week-2/debug/author.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 
+//author is an object, and the for...of loop is trying to iterate over it
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +13,18 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+// To access the values within an object, we can use various methods like Object.values(), Object.keys(), or Object.entries().
+
+for (const value of Object.values(author)) {
+  console.log(value);
+}
+
+// or
+for (const key of Object.keys(author)) {
+  console.log(author[key]);
+}
+
+// or
+for (const [key, value] of Object.entries(author)) {
   console.log(value);
 }
--- week-2/debug/recipe.js
+++ week-2/debug/recipe.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+//  it should log the title, the number it serves, and the ingredients for a recipe, with each ingredient on a new line.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -10,6 +11,9 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(
+  `${recipe.title} \nserves ${
+    recipe.serves
+  }\ningredients:\n${recipe.ingredients.join("\n")}`
+);
+//\n creates new lines in the output. It places title, serves, and ingredients on separate lines to make the console output more readable.
