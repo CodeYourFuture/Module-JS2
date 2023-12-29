bec451125db88b3b825da35a068990bec23ea76f
zeliha pala <zelihapala78@gmail.com>
2023-12-29 22:09:28 +0000

    checked

--- week-2/implement/tally.test.js
+++ week-2/implement/tally.test.js
@@ -12,20 +12,36 @@
  * tally(['a','a','b','c']), target output: { a : 2, b: 1, c: 1 }
  */
 
+const tally = require("./tally.js");
 // Acceptance criteria:
-
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
 
-// Given an empty array
-// When passed to tally
-// Then it should return an empty object
+describe("tally function", () => {
+  // Given an empty array
+  // When passed to tally
+  // Then it should return an empty object
+  test("returns an empty object for an empty array", () => {
+    const result = tally([]);
+    expect(result).toEqual({});
+  });
 
-// Given an array with duplicate items
-// When passed to tally
-// Then it should return counts for each unique item
+  // Given an array with duplicate items
+  // When passed to tally
+  // Then it should return counts for each unique item
 
-// Given an invalid input like a string
-// When passed to tally
-// Then it should throw an error
+  test("returns counts for each unique item in the array", () => {
+    const result = tally(["a", "a", "b", "c"]);
+    expect(result).toEqual({ a: 2, b: 1, c: 1 });
+  });
+
+  // Given an invalid input like a string
+  // When passed to tally
+  // Then it should throw an error
+  test("throws an error for invalid input like a string", () => {
+    expect(() => {
+      tally("invalid input");
+    }).toThrow();
+  });
+});
