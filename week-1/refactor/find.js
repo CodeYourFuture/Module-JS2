7e7266867fb53c4fbcb01531a5423c7c97e94f02
zeliha pala <zelihapala78@gmail.com>
2024-01-01 00:18:22 +0000

    ADDED FOR  ... OF LOOP

--- week-1/refactor/find.js
+++ week-1/refactor/find.js
@@ -11,3 +11,11 @@ function find(list, target) {
 }
 
 module.exports = find;
+
+/* let index = 0;
+for (const element of list) {
+  if (element === target) {
+    return index;
+  }
+  index++;
+} */
