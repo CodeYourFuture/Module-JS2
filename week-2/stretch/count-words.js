217164f05ea96a71ae6d30c53f7b47bec8b7b111
zeliha pala <zelihapala78@gmail.com>
2024-01-01 00:35:19 +0000

    count words done

--- week-2/stretch/count-words.js
+++ week-2/stretch/count-words.js
@@ -26,3 +26,26 @@
 
 3. Order the results to find out which word is the most common in the chapter
 */
+
+function countWords(str) {
+  const words = str
+    .toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g, "")
+    .split(" "); //removes specific punctuation characters from the string
+  const wordCount = {};
+
+  for (const word of words) {
+    if (wordCount[word]) {
+      wordCount[word]++;
+    } else {
+      wordCount[word] = 1;
+    }
+  }
+
+  return wordCount;
+}
+
+const result = countWords(
+  "Every now and then, usually between 8 p.m. and 10 p.m., Jacques finds himself transforming into a small furry rodent with a bushy tail.On one hand, Jacques is quite glad that he does not have classic lycanthropy. Turning into a squirrel does cause fewer problems than turning into a wolf. Instead of having to worry about accidentally eating the neighbor (that would be awkward), he  worries about being eaten by the neighbor’s cat. After two occasions where he woke up on a precariously thin branch in the crown of an oak, naked and disoriented, he has taken to locking the doors and windows of his room at night and putting a few walnuts on the floor to keep himself busy."
+);
+console.log(result);
