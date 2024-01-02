15c0c1a5df770b05115053a6df9515f079b3b308
Pedram Amani <amaaanip@gmail.com>
2024-01-02 17:53:41 +0000

    JS2-week1 | dedupe.js & dedupe.test.js done

--- week-1/implement/dedupe.js
+++ week-1/implement/dedupe.js
@@ -1,1 +1,15 @@
-function dedupe() {}
+function dedupe(arr) {
+  const deduplicatedArray = [];
+
+  for (const item of arr) {
+    if (!deduplicatedArray.includes(item)) {
+      deduplicatedArray.push(item);
+    }
+  }
+
+  return deduplicatedArray;
+}
+
+console.log(dedupe(["a", "a", "a", "b", "b", "c"]));
+
+module.exports = dedupe;
