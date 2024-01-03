5ff091f3224d6fac28a7f778e375e64c3677555b
Pedram Amani <amaaanip@gmail.com>
2024-01-03 16:08:53 +0000

    JS2-week2 | address.js fixed

--- week-2/debug/address.js
+++ week-2/debug/address.js
@@ -12,4 +12,13 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+// console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
+console.log(`My house number is ${address["houseNumber"]}`);
+
+// Before fixing the bug, the output would be "My house number is undefined".
+// This is because the code attempts to access the houseNumber property using array-like syntax (address[0]),
+// but the 'address' object does not have a property with the name 0.
+// As a result, it will return undefined.
+// The correct way to access the houseNumber property is using dot notation,
+// or square bracket notation with the correct property name (address.houseNumber).
