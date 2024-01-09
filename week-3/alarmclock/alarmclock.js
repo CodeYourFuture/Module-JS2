0a9b3931af5c8a01424a562826b88c0d9dffc572
Bakhat Begum <bakhtbagum@gmail.com>
2024-01-09 16:50:26 +0000

    removed codes

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,37 +1,6 @@
-function setAlarm() {
-  const timeElement = document.getElementById("timeRemaining");
+function setAlarm() {}
 
-      // Get the current time and parse it into minutes and seconds
-      let currentTime = timeElement.textContent.split(":");
-      let minutes = currentTime[0];
-      let seconds = currentTime[1];
-
-      // Update seconds (for example, increase by 10 seconds)
-       seconds += 5;
-
-      // Ensure that seconds don't exceed 59
-      if (seconds >= 60) {
-        seconds = 0;
-        minutes += 1;
-      }
-
-      // Format minutes and seconds to two digits
-      let formattedMinutes = String(minutes).padStart(2, "0");
-      let formattedSeconds = String(seconds).padStart(2, "0");
-
-      // Update the time in the h1 element
-      timeElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
-    }
-
-    // Add event listener to the button
-    const btn = document.getElementById("set");
-    btn.addEventListener("click", setAlarm);
-
-  
-
-
-
-// // DO NOT EDIT BELOW HERE
+// DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
@@ -54,6 +23,3 @@ function pauseAlarm() {
 }
 
 window.onload = setup;
-
-
-
