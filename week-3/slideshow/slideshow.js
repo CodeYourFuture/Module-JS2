74a634cea4cea65673c34af25b77eae5eeba7935
Pedro <riccip@outlook.com>
2024-01-09 21:34:39 +0000

    level 2 challenge done

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -9,6 +9,21 @@ const forwardButton = document.getElementById("forward-btn");
 const backwardButton = document.getElementById("backward-btn");
 
 let actualImg = 0;
+let intervalId
+
+function disableAllButtons() {
+    const allButtons = document.querySelectorAll('button');
+    allButtons.forEach(button => {
+        button.id !== 'stop' ? button.disabled = true : button.disabled = false;
+    });
+}
+
+function enableAllButtons() {
+    const allButtons = document.querySelectorAll('button');
+    allButtons.forEach(button => {
+        button.disabled = false;
+    });
+}
 
 function handleBackward() {
     actualImg - 1 < 0 ? actualImg = images.length - 1 : actualImg--;
@@ -20,8 +35,26 @@ function handleForward() {
     imageContainer.src = images[actualImg];
 }
 
+function handleAutoBackward() {
+    disableAllButtons()
+    intervalId = setInterval(handleBackward, 2000);
+}
+
+function handleAutoForward() {
+    disableAllButtons()
+    intervalId = setInterval(handleForward, 2000);
+}
+
+function handleStop() {
+    enableAllButtons()
+    clearInterval(intervalId);
+}
+
 
 window.onload = function () {
     document.querySelector('#forward-btn').addEventListener('click', handleForward);
     document.querySelector('#backward-btn').addEventListener('click', handleBackward);
+    document.querySelector('#auto-backward').addEventListener('click', handleAutoBackward);
+    document.querySelector('#auto-forward').addEventListener('click', handleAutoForward);
+    document.querySelector('#stop').addEventListener('click', handleStop);
 }
