91bed7b961126eb08d6f086b32e43bea7947821c
OrlandoMoralesKuan <omoralesk@hotmail.com>
2024-01-09 21:52:43 +0000

    AlarmClock app is finished

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,4 +1,30 @@
-function setAlarm() {}
+var myVar;
+var timer = document.getElementById("alarmSet");
+var countDownSeconds;
+startTime();
+
+function startTime() {
+  myVar = setInterval(start, 1000);
+  document.getElementById("timer").innerHTML = timer.value;
+  countDownSeconds = timer.value;
+}
+
+function start() {
+  countDownSeconds--;
+  document.getElementById("timer").innerHTML = countDownSeconds;
+  if (countDownSeconds == -1) {
+    stop();
+    document.getElementById("timer").innerHTML = "0";
+  } else if (countDownSeconds === 0) {
+    playAlarm();
+  }
+}
+
+function stop() {
+  clearInterval(myVar);
+}
+
+//function setAlarm() { }
 
 // DO NOT EDIT BELOW HERE
 
