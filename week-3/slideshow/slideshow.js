15281c33e07bd33e742b20230759772647cf3489
Pedro <riccip@outlook.com>
2024-01-09 21:03:05 +0000

    forward and backward functions implemented

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -4,5 +4,24 @@ const images = [
     "./assets/cute-cat-c.jpg",
 ];
 
+const imageContainer = document.getElementById("carousel-img");
+const forwardButton = document.getElementById("forward-btn");
+const backwardButton = document.getElementById("backward-btn");
 
-// Write your code here
+let actualImg = 0;
+
+function handleBackward() {
+    actualImg + 1 == images.length ? actualImg = 0 : actualImg++;
+    imageContainer.src = images[actualImg];
+}
+
+function handleForward() {
+    actualImg - 1 < 0 ? actualImg = images.length - 1 : actualImg--;
+    imageContainer.src = images[actualImg];
+}
+
+
+window.onload = function() {
+    document.querySelector('#forward-btn').addEventListener('click', handleForward);
+    document.querySelector('#backward-btn').addEventListener('click', handleBackward);
+}
