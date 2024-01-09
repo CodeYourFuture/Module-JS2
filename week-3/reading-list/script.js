d8e3cec9d0407189b2257fc9087e30148666ff7d
Pedro <riccip@outlook.com>
2024-01-09 17:29:19 +0000

    add books in the ul done

--- week-3/reading-list/script.js
+++ week-3/reading-list/script.js
@@ -1,3 +1,34 @@
+function loadBooks() {
+  let booksList = document.querySelector("#reading-list");
+
+  books.forEach(book => {
+    const title = book.title;
+    const author = book.author;
+    const alreadyRead = book.alreadyRead;
+    const bookCoverImage = book.bookCoverImage;
+    console.log(title);
+    console.log(author);
+    console.log(alreadyRead);
+    console.log(bookCoverImage);
+
+    let htmlLi = document.createElement("li");
+    let htmlPTitle = document.createElement("p");
+    let htmlPAuthor = document.createElement("p");
+    let htmlImg = document.createElement("img");
+
+    booksList.appendChild(htmlLi);
+    htmlLi.setAttribute("alreadyRead", alreadyRead)
+
+    htmlLi.appendChild(htmlPTitle);
+    htmlPTitle.textContent = title;
+    htmlLi.appendChild(htmlPAuthor);
+    htmlPAuthor.textContent = author;
+    htmlLi.appendChild(htmlImg);
+    htmlImg.setAttribute("src", bookCoverImage);
+  });
+}
+
+window.onload = loadBooks
 // for the tests, do not modify this array of books
 const books = [
   {
