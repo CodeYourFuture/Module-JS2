7ea671e2d4d06d3df81c15b2f1a5bdc5b38f8f29
Pedro <riccip@outlook.com>
2024-01-09 15:23:32 +0000

    timer and alarm sound implemented

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,4 +1,29 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeRemaining = document.querySelector('#timeRemaining')
+  let alarmTimeInput = document.querySelector('#alarmSet').value
+  let alarmTimeRemaining = alarmTimeInput
+
+  timeRemaining.innerHTML = `Time Remaining: ${convertSecondsToMinutes(alarmTimeRemaining)}`;
+  console.log(convertSecondsToMinutes(alarmTimeRemaining));
+
+  let intervalAlarmId = setInterval(() => {
+    alarmTimeRemaining--;
+    console.log(convertSecondsToMinutes(alarmTimeRemaining));
+    timeRemaining.innerHTML = `Time Remaining: ${convertSecondsToMinutes(alarmTimeRemaining)}`;
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(intervalAlarmId); // Stop the interval
+    playAlarm()
+  }, alarmTimeInput * 1000);
+}
+
+function convertSecondsToMinutes(seconds) {
+  let minutes = Math.trunc(seconds / 60).toLocaleString('en-UK', { minimumIntegerDigits: 2, useGrouping: false });
+  let secondsRemaining = (seconds % 60).toLocaleString('en-UK', { minimumIntegerDigits: 2, useGrouping: false });
+
+  return `${minutes}:${secondsRemaining}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
