bc507d7d3121495fa9bffe6c40629b690a20bfcf
Bakhat Begum <bakhtbagum@gmail.com>
2024-01-10 22:49:48 +0000

    added functions and element in html

--- week-3/reading-list/script.js
+++ week-3/reading-list/script.js
@@ -1,18 +1,17 @@
-// for the tests, do not modify this array of books
+// // for the tests, do not modify this array of books
 
-function readingList(li){
-return li[Math.floor(Math.random() * li.length)];
+function readingList(li) {
+  // Shallow copy the array to avoid modifying the original array
+  const shuffledBooks = [...li];
+
+  // Shuffle the array using the Fisher-Yates algorithm
+  for (let i = shuffledBooks.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledBooks[i], shuffledBooks[j]] = [shuffledBooks[j], shuffledBooks[i]];
+  }
+
+  return shuffledBooks;
 }
-
-
-function bookTitle(event){
-  const name = document.getElementById("titleName");
-  const reading = readingList(books);
-  name.innerText = reading.title;
-  const getAuthor = document.getElementById("author");
-  getAuthor.innerText = reading.author;
-  };
-
 
 const books = [
   {
@@ -35,4 +34,26 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
+
+function bookTitle() {
+  const shuffledBooks = readingList(books);
+  console.log(shuffledBooks);
+
+  // Iterate through each list item and apply the appropriate class
+  const listItems = document.querySelectorAll(".order-list");
+
+  for (let i = 0; i < listItems.length; i++) {
+    const currentItem = listItems[i];
+    currentItem.classList.add(shuffledBooks[i].alreadyRead ? "read" : "unread");
+
+    // Update title, author, and cover for each book
+    currentItem.querySelector(".title").innerText = shuffledBooks[i].title;
+    currentItem.querySelector(".author").innerText = shuffledBooks[i].author;
+    currentItem.querySelector(".cover").src = shuffledBooks[i].bookCoverImage;
+  }
+}
+
+// Call the function to display a random combination of books
+bookTitle();
+
 
