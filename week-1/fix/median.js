cc6bb33942fa65053475043bd6b54017a48a069d
zeliha pala <zelihapala78@gmail.com>
2024-01-12 19:07:08 +0000

    self study

--- week-1/fix/median.js
+++ week-1/fix/median.js
@@ -14,9 +14,9 @@ module.exports = calculateMedian; */
 // function should be to calculate the median value of a given list, but the calculateMedian function isn't actually computing the median of the list. Instead of finding the median element of the list, this function is extracting the element from the middle index of the list and returning it.
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor((list.length - 1) / 2);
+  const middleIndex = Math.floor(list.length / 2);
   if (list.length % 2 === 0) {
-    return (list[middleIndex] + list[middleIndex + 1]) / 2;
+    return (list[middleIndex - 1] + list[middleIndex]) / 2;
   } else {
     return list[middleIndex];
   }
@@ -25,3 +25,11 @@ module.exports = calculateMedian;
 
 const listExample = [2, 3, 4, 5, 6, 7, 8];
 console.log(calculateMedian(listExample));
+
+//for me : Function is defined, taking an array parameter named 'list'
+// this const middleIndex = Math.floor(list.length / 2); Calculates the index of the median element by dividing the length of the list by 2 and flooring the result.
+
+//If the length of the list is even (remainder of division by 2 is 0),
+
+// returns (list[middleIndex - 1] + list[middleIndex]) / 2; this  Calculates and returns the average of the two middle elements.
+// If the length of the list is odd, returns the value of the middle element.
