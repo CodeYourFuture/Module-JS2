d2b604492c8b951a3b79a44610ebc3f9dcf4a22a
OrlandoMoralesKuan <omoralesk@hotmail.com>
2024-01-12 17:35:01 +0000

    reading-list exercise is done

--- week-3/reading-list/script.js
+++ week-3/reading-list/script.js
@@ -20,3 +20,30 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
+
+function renderBooks() {
+  const container = document.body;
+
+  books.forEach((book) => {
+    const bookElement = document.createElement("li");
+    bookElement.classList.add("book", book.alreadyRead ? "read" : "not-read");
+
+    const titleElement = document.createElement("h3");
+    titleElement.textContent = book.title;
+
+    const authorElement = document.createElement("p");
+    authorElement.textContent = `By ${book.author}`;
+
+    const imageElement = document.createElement("img");
+    imageElement.src = book.bookCoverImage;
+    imageElement.alt = book.title;
+
+    bookElement.appendChild(titleElement);
+    bookElement.appendChild(authorElement);
+    bookElement.appendChild(imageElement);
+
+    container.appendChild(bookElement);
+  });
+}
+
+renderBooks();
