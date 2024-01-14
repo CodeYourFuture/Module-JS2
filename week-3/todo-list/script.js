e7484afd06838711960906b047987b31c4b3026f
bahador <noor.bahadory786@gmail.com>
2024-01-14 01:42:35 +0000

    changed code based on feedback

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -1,6 +1,9 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// Load the existing data when the page loads
+window.addEventListener("load", showList);
+
 function populateTodoList() {
   if (inputBox.value === "") {
     alert("You must write a task");
@@ -10,52 +13,35 @@ function populateTodoList() {
     listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "🗑";
-    // cross code \u00d7
     li.appendChild(span);
   }
   inputBox.value = "";
-  // after type in input it should  kep empty the input filed
   saveData();
 }
-// writing test for click function
-listContainer.addEventListener(
-  "click",
-  function (e) {
-    if (e.target.tagName === "LI") {
-      //to check if the tag name of a target element is "LI" (list item)
-      e.target.classList.toggle("checked");
-      // we call the toggle function to toggle if the chec
-      saveData();
-    } else if (e.target.tagName === "SPAN") {
-      e.target.parentElement.remove();
-      saveData();
-    }
-  },
-  false
-);
 
-// we save our data in the local storage through saveData function
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+});
+
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
-// to display data we write localStorage.getItem("data")
-
-// display the data whenever we open or refresh the browser
 
 function showList() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
-//showList();
-// calling the function
 
 function deleteList() {
-  // Get the reference to the ul element
   var list = document.querySelector(".checked");
-
-  // Check if the ul element exists
   if (list) {
-    // Remove the ul element from its parent
     list.parentNode.removeChild(list);
+    saveData();
   } else {
     console.log("UL element not found");
   }
