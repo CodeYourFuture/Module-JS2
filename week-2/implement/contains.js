7b45aadedc8bcd7691c527e25b2032897437fd15
RbAvci <rb.avci@gmail.com>
2024-01-15 10:52:29 +0000

    implemented contains (hasOwnProperty)

--- week-2/implement/contains.js
+++ week-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(obj, key) {
+  if (typeof obj === "object" && Object.hasOwnProperty.call(obj, key)) {
+    return true;
+  }
+  return false;
+}
 
 module.exports = contains;
--- week-2/implement/contains.test.js
+++ week-2/implement/contains.test.js
@@ -17,18 +17,53 @@ as the object doesn't contains a key of 'c'
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
 
+test("if the object contains the property turn true", () => {
+  expect(contains({ a : 1, b: 2 },'a')).toBe(true);
+});
+
+
+
+test("if the object doesn't contains the property turn false", () => {
+  expect(contains({ a: 1, b: 2 }, 'c')).toBe(false);
+});
+
+
 // Given an empty object
 // When passed to contains
 // Then it should return false
+
+test("if the object is empty, turn false", () => {
+  expect(contains({ }, 'c')).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
 
+test("When passed to contains with an existing property name should turn true", () => {
+  expect(contains({ a: 1, b: 2 }, "a")).toBe(true);
+});
+
+
+
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
 
+test("When passed to contains with a non-existent property name, turn false", () => {
+  expect(contains({ a: 1, b: 2 }, "c")).toBe(false);
+});
+
+
 // Given invalid parameters like arrays
 // When passed to contains
 // Then it should return false or throw an error
+
+test("When passed to contains invalid parameters like arrays, turn false", () => {
+  expect(contains([], "c")).toBe(false);
+  expect(contains(2, "c")).toBe(false);
+  expect(contains(NaN, "c")).toBe(false);
+  expect(contains(true, "c")).toBe(false);
+  expect(contains(false, "c")).toBe(false);
+  expect(contains(undefined, "c")).toBe(false);
+});
