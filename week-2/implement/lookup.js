0497ad8f0296cc2ea62073298274d8611a3311a5
RbAvci <rb.avci@gmail.com>
2024-01-15 11:40:39 +0000

    implemented lookup / reaching pairs of array

--- week-2/implement/lookup.js
+++ week-2/implement/lookup.js
@@ -1,17 +1,11 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyArray) {
+  const lookup = {};
+
+  for (let element of countryCurrencyArray) {
+    lookup[element[0]]= element[1];
+  }
+  return lookup;
 }
 
-/* ======= Test suite is provided below... =====
- */
 
-test("converts a single pair of currency codes", () => {
-  expect(createLookup([["GB", "GBP"]])).toEqual({
-    GB: "GBP",
-  });
-  expect(createLookup([["DE", "EUR"]])).toEqual({
-    DE: "EUR",
-  });
-});
-
-test.todo("creates a country currency code lookup for multiple codes");
+module.exports = createLookup;
