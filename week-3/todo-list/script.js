2fef3056531450e4b511cb91c81075957162d402
Bakhat Begum <bakhtbagum@gmail.com>
2024-01-15 15:12:07 +0000

    removed all style in javaScript and added in css file

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -7,40 +7,24 @@ let todos = [
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
- 
+
   let list = document.getElementById("todo-list");
   let listValue = list.value;
  
+  
   let li = document.createElement("li")
-  li.style.listStyle = "none";
-  li.style.position = "relative";
-  li.style.right = "600px";
-  li.style.bottom= "200px"
-  li.style.fontSize = "1.2em"
-  li.style.background = "white";
-  li.style.width = "70%";
-  li.style.padding = "20px";
-  li.style.borderRadius = "7px";
-  li.style.marginLeft = "15%";
-  li.style.marginBottom= "20px";
-  li.style.paddingLeft = "0px";
+  li.classList.add("style-list");
+  list.appendChild(li);
   
   let container = document.createElement("span")
     container.className = "badge bg-primary rounded-pill";
-    container.style.position = "absolute";
-    container.style.left = "20px";
-    container.style.paddingLeft = "300px";
-    container.style.top = "50%";
-    container.style.paddingTop = "20px";
-    container.style.transform = "translateY(-50%)";
+      container.classList.add("layout");
+      list.appendChild(container);
 
+      
     let checkIcon = document.createElement("i");
     checkIcon.className = 'fa fa-check';
     checkIcon.setAttribute("aria-hidden", "true");
-    checkIcon.style.color = "white";
-    checkIcon.style.background = "blue";
-    checkIcon.style.width= "50px";
-    checkIcon.style.marginRight = "10px";
     checkIcon.onclick = function() {
       li.classList.toggle("completed");
     }
@@ -49,9 +33,6 @@ function addNewTodo(event) {
     let trashIcon = document.createElement("i");
     trashIcon.className = 'fa fa-trash';
     trashIcon.setAttribute("aria-hidden", "true");
-    trashIcon.style.color = "black";
-    // trashIcon.style.position = "apsolute";
-    trashIcon.style.paddingRight= "20px";;
     trashIcon.onclick = function(){
       li.remove();
    };
@@ -66,8 +47,7 @@ function addNewTodo(event) {
   ul.appendChild(li);
 
   list.value = "";
-
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
+   event.preventDefault();
 }
 
 
@@ -75,7 +55,6 @@ function addNewTodo(event) {
 function deleteAllCompletedTodos(event) {
   let todoList = document.getElementById("task-list");
   let completedItems = document.querySelectorAll(".completed");
-
   completedItems.forEach(function (item) {
     todoList.removeChild(item);
   });
