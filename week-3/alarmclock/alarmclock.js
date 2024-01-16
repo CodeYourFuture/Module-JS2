b5f79f241d758e4cd4dbc4054a524ab2c1720ac8
OrlandoMoralesKuan <omoralesk@hotmail.com>
2024-01-16 10:47:49 +0000

    alarmclock javascript is done

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,4 +1,50 @@
-function setAlarm() {}
+var countdownInterval;
+var secondsRemaining = 0;
+
+function setAlarm() {
+  const alarmSetInput = document.getElementById("alarmSet");
+  const timeRemainingElemet = document.getElementById("timeRemaining");
+
+  const secondsInput = parseInt(alarmSetInput.value, 10);
+
+  if (isNaN(secondsInput) || secondsInput <= 0) {
+    alert("Please insert a valid positive number");
+    return;
+  }
+
+  secondsRemaining = secondsInput;
+  updateCountdown();
+  countdownInterval = setInterval(updateCountdown, 1000);
+}
+
+function updateCountdown() {
+  const timeRemainingElemet = document.getElementById("timeRemaining");
+
+  if (secondsRemaining <= 0) {
+    clearInterval(countdownInterval);
+    playAlarm();
+    timeRemainingElemet.textContent = "Time Remaining: 00:00";
+  } else {
+    const minutes = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60;
+
+    timeRemainingElemet.textContent = `Time Remaining: ${padNumber(
+      minutes
+    )}:${padNumber(seconds)}`;
+    secondsRemaining--;
+  }
+}
+
+function padNumber(number) {
+  return number.toString().padStart(2, "0");
+}
+
+function stopAlarm() {
+  clearInterval(countdownInterval);
+  pauseAlarm();
+  const timeRemainingElemet = document.getElementById("timeRemaining");
+  timeRemainingElemet.textContent = "Time Remaining: 00:00";
+}
 
 // DO NOT EDIT BELOW HERE
 
