3a01c0eefb1f2b705176f6a739cf85a65dcaf250
RbAvci <rb.avci@gmail.com>
2024-01-17 22:41:05 +0000

    implemented forward and backward actions

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -1,18 +1,35 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
-    "./assets/cute-cat-d.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-d.jpg",
 ];
-
 
 // Write your code here
 
+let index = 0;
+const carouselImage = document.getElementById("carousel-img");
+const imageIndex = document.getElementById("img-index");
+
 const forwardBtn = document.getElementById("forward-btn");
-forwardBtn.addEventListener("click", function(){
-    console.log('clicked me!');
+forwardBtn.addEventListener("click", function () {
+  index++;
+  if (index == images.length) {
+    index = 0;
+  }
+  setCarouselImage();
 });
+
 const backwardsBtn = document.getElementById("backward-btn");
-backwardsBtn.addEventListener("click", function(){
-    console.log('clicked me!');
+backwardsBtn.addEventListener("click", function () {
+  index--;
+  if (index == -1) {
+    index = images.length - 1;
+  }
+  setCarouselImage();
 });
+
+function setCarouselImage() {
+  carouselImage.src = images[index];
+  imageIndex.innerText = index;
+}
