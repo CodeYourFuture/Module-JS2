1b4f36c3984eb7d19aff4596e3d1fb02baf463b8
Sabella-8 <sabella549@gmail.com>
2024-01-17 15:13:26 +0000

    week js2 week 1 correction

--- week-1/implement/max.js
+++ week-1/implement/max.js
@@ -1,23 +1,22 @@
-function maxNumber(num) {
-    if (num.length === 0) {
+function maxNumber(inputNumber) {
+    if (inputNumber.length === 0) {
         return -Infinity; 
     }
 
-    if (num.length === 1 && typeof num === 'number') {
-        return num;
+    if (inputNumber.length === 1 && typeof inputNumber === 'number') {
+        return inputNumber;
     }
 
     let maxNum = 0;
 
-    for (let i = 0; i < num.length; i++) {
-     if(typeof num[i]!=='number'){continue;}
-        if (num[i] >= maxNum) {
-            maxNum = num[i];
+    for (let i = 0; i < inputNumber.length; i++) {
+     if(typeof inputNumber[i]!=='number'){continue;}
+        if (inputNumber[i] >= maxNum) {
+            maxNum = inputNumber[i];
         }
     }
 
     return maxNum;
 }
-console.log(maxNumber([])); 
-console.log(maxNumber(['hey', 10, 'hi', 60, 10]));
+
 module.exports = maxNumber;
