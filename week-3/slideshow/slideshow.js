71c2fcc4ebf295fce681fe9f7c1f50ff2b21cb3f
RbAvci <rb.avci@gmail.com>
2024-01-17 22:53:35 +0000

    implemented autoForward action

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -8,28 +8,45 @@ const images = [
 // Write your code here
 
 let index = 0;
+const interval = 2000;
 const carouselImage = document.getElementById("carousel-img");
 const imageIndex = document.getElementById("img-index");
 
 const forwardBtn = document.getElementById("forward-btn");
 forwardBtn.addEventListener("click", function () {
-  index++;
-  if (index == images.length) {
-    index = 0;
-  }
-  setCarouselImage();
+  nextImage();
 });
 
 const backwardsBtn = document.getElementById("backward-btn");
 backwardsBtn.addEventListener("click", function () {
-  index--;
-  if (index == -1) {
-    index = images.length - 1;
-  }
-  setCarouselImage();
+    previousImage();
 });
 
 function setCarouselImage() {
   carouselImage.src = images[index];
   imageIndex.innerText = index;
 }
+
+function nextImage (){
+    index++;
+    if (index == images.length) {
+      index = 0;
+    }
+    setCarouselImage();
+}
+
+function previousImage(){
+  index--;
+  if (index == -1) {
+    index = images.length - 1;
+  }
+  setCarouselImage();
+}
+
+
+const autoForwardBtn = document.getElementById("auto-forward-btn");
+autoForwardBtn.addEventListener("click", function () {
+    setInterval(() => {
+        nextImage();
+    }, interval);
+});
