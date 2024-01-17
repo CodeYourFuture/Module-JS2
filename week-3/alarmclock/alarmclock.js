ffcdc774d04c1462039679aff65daaf6eac74fe1
RbAvci <rb.avci@gmail.com>
2024-01-17 00:38:58 +0000

    implemented alarmclock

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,4 +1,54 @@
-function setAlarm() {}
+let intervalId;
+let bgIntervalId;
+
+function setAlarm() {
+  clearInterval(intervalId);
+  clearInterval(bgIntervalId);
+  setBackground("none");
+  document.getElementById("alarmSet").classList.remove("invalidInput");
+
+  let alarmSet = document.getElementById("alarmSet").value;
+
+  if (alarmSet > 3599) {
+    document.getElementById("alarmSet").classList.add("invalidInput");
+    return;
+  }
+
+  setTime(alarmSet);
+
+  intervalId = setInterval(function () {
+    alarmSet = alarmSet - 1;
+    setTime(alarmSet);
+    if (alarmSet == 0) {
+      setBackground("red", "white");
+      playAlarm();
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+
+function setBackground(color1, color2) {
+  let i = 0;
+
+  bgIntervalId = setInterval(function () {
+    if (i % 2 == 0) {
+      document.body.style.background = color1;
+    } else {
+      document.body.style.background = color2;
+    }
+    i++;
+  }, 500);
+}
+
+function setTime(time) {
+  document.getElementById("time").innerText = toTime(time);
+}
+
+function toTime(seconds) {
+  var date = new Date(null);
+  date.setSeconds(seconds);
+  return date.toISOString().slice(14, 19);
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -11,6 +61,8 @@ function setup() {
 
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
+    clearInterval(bgIntervalId);
+    setBackground("none");
   });
 }
 
