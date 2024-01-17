9e5f189b98a7fa8f4e88ca193fd5608acb4ebdc3
RbAvci <rb.avci@gmail.com>
2024-01-17 22:16:25 +0000

    add listener to buttons

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -2,7 +2,17 @@ const images = [
     "./assets/cute-cat-a.png",
     "./assets/cute-cat-b.jpg",
     "./assets/cute-cat-c.jpg",
+    "./assets/cute-cat-d.jpg",
 ];
 
 
 // Write your code here
+
+const forwardBtn = document.getElementById("forward-btn");
+forwardBtn.addEventListener("click", function(){
+    console.log('clicked me!');
+});
+const backwardsBtn = document.getElementById("backward-btn");
+backwardsBtn.addEventListener("click", function(){
+    console.log('clicked me!');
+});
