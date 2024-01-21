f788673daf3e393412944a0155b0d38d48413f74
Sabella-8 <sabella549@gmail.com>
2024-01-21 17:12:30 +0000

    alarm-clock

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,4 +1,39 @@
-function setAlarm() {}
+
+
+function setAlarm() {
+  const inputTime = document.getElementById("alarmSet");
+  const spanTime = document.querySelector("span");
+  const inputValue = inputTime.value;
+  spanTime.textContent = timeFormat(inputValue);
+}
+
+function timeFormat(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSec = seconds % 60;
+  const formattedMin = String(minutes).padStart(2, "0");
+  const formattedSec = String(remainingSec).padStart(2, "0");
+  return `${formattedMin}:${formattedSec}`;
+}
+
+setAlarm();
+
+setInterval(() => {
+  const spanTime = document.querySelector("span");
+  if (spanTime.textContent === "00:00") {
+    // playAlarm();
+
+    // stop();
+  } else if (spanTime.textContent != "00:00") {
+    const currentSeconds =
+      parseInt(spanTime.textContent.slice(3), 10) +
+      parseInt(spanTime.textContent.slice(0, 2)) * 60;
+
+    spanTime.textContent = timeFormat(currentSeconds - 1);
+    // pauseAlarm();
+  }
+}, 1000);
+
+
 
 // DO NOT EDIT BELOW HERE
 
