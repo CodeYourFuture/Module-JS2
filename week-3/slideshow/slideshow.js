6aaceae30d8bce5f0a2d59e9618d9352695319c5
Bakhat Begum <bakhtbagum@gmail.com>
2024-01-23 15:18:00 +0000

    removed let img_list from code

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -1,7 +1,7 @@
-let img_list;
+
 
 let element_image = document.getElementById('image');
- img_list = [
+ let img_list = [
   "./assets/cute-cat-a.png",
   "./assets/cute-cat-b.jpg",
    "./assets/cute-cat-c.jpg",
@@ -9,20 +9,18 @@ let element_image = document.getElementById('image');
   ];
 element_image.setAttribute("src", img_list[0]);
 
-
-function add_carousel() {
-    let element_imageI = document.getElementById('image');
-    element_imageI.setAttribute("src", img_list.slice(-1)[0]);
- 
+function add_carousel(){
+  let element_imageI = document.getElementById('image');
+  element_imageI.setAttribute("src", img_list.slice(-1)[0]);
 }
 
-function carousel() {
-  if (img_list) {
-    let element_imageJ = document.getElementById('image');
+function carousel(){
+  if(img_list) {
+    let element_imageJ = document.getElementsById('image');
     element_imageJ.setAttribute("src", img_list[0]);
-  } else {
+  }else{
     let element_imageK = document.getElementById('image');
-    element_imageK.setAttribute("src", img_list.slice(-1)[0]);
+    element_imageK.setAttribute("src", img_list.slice(-1)[0])
   }
 }
 
