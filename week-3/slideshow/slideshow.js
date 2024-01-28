bd73e06ab4d4ff9079e57ac2c41d99142101a1c0
fikret ellek <fikret.ellek@gmail.com>
2024-01-28 16:52:45 +0000

    level 1 implemented

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -1,8 +1,39 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
+let currentPhotoIndex = 1;
+
+const image = document.getElementById("carousel-img");
+
+const backwardBtn = document.getElementById("backward-btn");
+const forwardBtn = document.getElementById("forward-btn");
 
 // Write your code here
+
+function generatePhoto() {
+  image.setAttribute("src", images[currentPhotoIndex]);
+}
+
+function nextPhoto() {
+  ++currentPhotoIndex;
+  if (currentPhotoIndex === 3) {
+    currentPhotoIndex = 0;
+  }
+  generatePhoto();
+}
+
+function previousPhoto() {
+  --currentPhotoIndex;
+  if (currentPhotoIndex === -1) {
+    currentPhotoIndex = 2;
+  }
+  generatePhoto();
+}
+
+backwardBtn.addEventListener("click", previousPhoto);
+forwardBtn.addEventListener("click", nextPhoto);
+
+window.onload = generatePhoto;
