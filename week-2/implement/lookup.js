c2704e5107b43698dcc696d39b6fe56c4798d63d
Kirill Kovzel <kirill.kovzel@gmail.com>
2024-07-22 12:46:56 +0100

    Move test related content to lookup.test.js

--- week-2/implement/lookup.js
+++ week-2/implement/lookup.js
@@ -1,17 +1,3 @@
 function createLookup() {
   // implementation here
 }
-
-/* ======= Test suite is provided below... =====
- */
-
-test("converts a single pair of currency codes", () => {
-  expect(createLookup([["GB", "GBP"]])).toEqual({
-    GB: "GBP",
-  });
-  expect(createLookup([["DE", "EUR"]])).toEqual({
-    DE: "EUR",
-  });
-});
-
-test.todo("creates a country currency code lookup for multiple codes");
